3d63dea815fc1252c6b69cc6369d11e04a234710
sardhani <sardhani@companieshouse.gov.uk>
2024-01-22 10:02:51 +0000

    soleTrader captureCorrespondanceAddressManual

--- src/lib/utils/error_manifests/dateOfBirth.ts
+++ src/lib/utils/error_manifests/dateOfBirth.ts
@@ -52,6 +52,74 @@ const dateOfBirthErrorManifest = {
         tooYoung: {
             summary: "You must be at least 16 years old",
             inline: "You must be at least 16 years old"
+        },
+        noPropertyDetails: {
+            summary: "Enter a property name or number",
+            inline: "Enter a property name or number"
+        },
+        noAddressLine1: {
+            summary: "Enter an address",
+            inline: "Enter an address"
+        },
+        noCityOrTown: {
+            summary: "Enter a city or town",
+            inline: "Enter a city or town"
+        },
+        noPostCode: {
+            summary: "Enter a postcode or ZIP",
+            inline: "Enter a postcode or ZIP"
+        },
+        invalidPropertyDetails: {
+            summary: "Property name or number must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes",
+            inline: "Property name or number must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes"
+        },
+        invalidAddressLine1: {
+            summary: "Address line 1 must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes",
+            inline: "Address line 1 must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes"
+        },
+        invalidAddressLine2: {
+            summary: "Address line 2 must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes",
+            inline: "Address line 2 must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes"
+        },
+        invalidAddressTown: {
+            summary: "City or town must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes",
+            inline: "City or town must only include letters a to z, numbers and common special characters such as hyphens, spaces and apostrophes"
+        },
+        invalidAddressCounty: {
+            summary: "County or region must only include letters a to z",
+            inline: "County or region must only include letters a to z"
+        },
+        invalidAddressCountry: {
+            summary: "Country must only include letters a to z",
+            inline: "Country must only include letters a to z"
+        },
+        invalidAddressPostcode: {
+            summary: "Enter a full UK postcode",
+            inline: "Enter a full UK postcode"
+        },
+        invalidPropertyDetailsLength: {
+            summary: "Property name or number must be 200 characters or less",
+            inline: "Property name or number must be 200 characters or less"
+        },
+        invalidAddressLine1Length: {
+            summary: "Address line 1 must be 50 characters or less",
+            inline: "Address line 1 must be 50 characters or less"
+        },
+        invalidAddressLine2Length: {
+            summary: "Address line 2 must be 50 characters or less",
+            inline: "Address line 2 must be 50 characters or less"
+        },
+        invalidAddressTownLength: {
+            summary: "City or town must be 50 characters or less",
+            inline: "City or town must be 50 characters or less"
+        },
+        invalidAddressCountyLength: {
+            summary: "County or region must be 50 characters or less",
+            inline: "County or region must be 50 characters or less"
+        },
+        invalidAddressCountryLength: {
+            summary: "Country must be 50 characters or less",
+            inline: "Country must be 50 characters or less"
         }
     }
 };
--- test/src/routers/soleTraderDateOfBirthRouter.test.ts
+++ test/src/routers/soleTraderDateOfBirthRouter.test.ts
@@ -20,3 +20,22 @@ describe("POST /sole-trader/date-of-birth", () => {
         await router.post("/sole-trader/date-of-birth").send({ dobDay: "32", dobMonth: "02", dobYear: "2010" }).expect(400);
     });
 });
+
+describe("GET /sole-trader/address-correspondance-manual", () => {
+    it("should return status 200", async () => {
+        await router.get("/sole-trader/address-correspondance-manual").expect(200);
+    });
+});
+
+// Test for correct form details entered, will return 400 after redirecting to the next page.
+describe("POST /sole-trader/address-correspondance-manual", () => {
+    it("should return status 400 after redirect", async () => {
+        await router.post("/sole-trader/address-correspondance-manual").send({ addressPropertyDetails: "abc", addressLine1: "pqr", addressTown: "lmn", addressPostcode: "MK9 3GB" }).expect(400);
+    });
+});
+// Test for incorrect form details entered, will return 400.
+describe("POST /sole-trader/address-correspondance-manual", () => {
+    it("should return status 400", async () => {
+        await router.post("/sole-trader/address-correspondance-manual").send({ addressPropertyDetails: "abc", addressPostcode: "MK913GB" }).expect(400);
+    });
+});
