2a32a416b42da70fc9e4bbd49d782739dcfd8e55
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-01-25 14:07:54 +0000

    Updated validation chain to only show a single message is field is empty

--- src/lib/validation/whereDoYouLive.ts
+++ src/lib/validation/whereDoYouLive.ts
@@ -3,5 +3,5 @@ import countryList from "../countryList";
 import whereDoYouLiveErrorManifest from "../utils/error_manifests/whereDoYouLive";
 
 export const whereDoYouLiveValidator = [
-    body("countryInput", whereDoYouLiveErrorManifest.validation.noData.summary).trim().notEmpty().isIn(countryList)
+    body("countryInput", whereDoYouLiveErrorManifest.validation.noData.summary).trim().notEmpty().bail().isIn(countryList)
 ];
