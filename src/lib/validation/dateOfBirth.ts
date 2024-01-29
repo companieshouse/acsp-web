81da2db08e9514a74d9fbf752d10872583ec11b3
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-01-29 10:25:58 +0000

    Refactored njk screen to use GDS component and updated the validation to use express validator

--- src/lib/validation/dateOfBirth.ts
+++ src/lib/validation/dateOfBirth.ts
@@ -1,122 +1,93 @@
+import { body } from "express-validator";
 import dateOfBirthErrorManifest from "../utils/error_manifests/dateOfBirth";
-import { GenericValidator } from "./generic";
 
-export class DateOfBirthValidator extends GenericValidator {
+export const dateOfBirthValidator = [
+    body("dob-day").custom((value, { req }) => dateDayChecker(req.body["dob-day"], req.body["dob-month"], req.body["dob-year"])),
+    body("dob-month").custom((value, { req }) => dateMonthChecker(req.body["dob-day"], req.body["dob-month"], req.body["dob-year"])),
+    body("dob-year").custom((value, { req }) => dateYearChecker(req.body["dob-day"], req.body["dob-month"], req.body["dob-year"])),
+    body("dob-day").custom((value, { req }) => validDataChecker(req.body["dob-day"], req.body["dob-month"], req.body["dob-year"]))
+];
 
-    constructor (classParam?: string) {
-        super();
-        this.errorManifest = dateOfBirthErrorManifest;
+const dateDayChecker = (day: string, month: string, year: string) => {
+    if (day.trim() === "" && month.trim() === "" && year.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noData.summary);
+    } else if (day.trim() === "" && month.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noDayMonth.summary);
+    } else if (day.trim() === "" && year.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noDayYear.summary);
+    } else if (day.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noDay.summary);
     }
+    return true;
+};
 
-    validateInputData (payload: any): Promise<any> {
-        try {
-            var dobDay = +payload.dobDay;
-            var dobMonth = +payload.dobMonth;
-            var dobYear = +payload.dobYear;
+const dateMonthChecker = (day: string, month: string, year: string) => {
+    if (day.trim() !== "" && month.trim() === "" && year.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noMonthYear.summary);
+    } else if (day.trim() !== "" && month.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noMonth.summary);
+    }
+    return true;
+};
 
-            if (payload.dobDay === "" && payload.dobMonth === "" && payload.dobYear === "") {
-                this.errors.stack.dobDay = this.errorManifest.validation.noData;
-            } else if (payload.dobDay === "" && payload.dobMonth === "") {
-                this.errors.stack.dobDay = this.errorManifest.validation.noDayMonth;
-            } else if (payload.dobDay === "" && payload.dobYear === "") {
-                this.errors.stack.dobDay = this.errorManifest.validation.noDayYear;
-            } else if (payload.dobMonth === "" && payload.dobYear === "") {
-                this.errors.stack.dobMonth = this.errorManifest.validation.noMonthYear;
-            } else if (payload.dobDay === "") {
-                this.errors.stack.dobDay = this.errorManifest.validation.noDay;
-            } else if (payload.dobMonth === "") {
-                this.errors.stack.dobMonth = this.errorManifest.validation.noMonth;
-            } else if (payload.dobYear === "") {
-                this.errors.stack.dobYear = this.errorManifest.validation.noYear;
-            } else if (!this.isNotTooYoung(dobDay, dobMonth, dobYear) && this.isValidYear(dobYear)) {
-                this.errors.stack.dobDay = this.errorManifest.validation.tooYoung;
-            } else if (!this.isNotInFuture(dobDay, dobMonth, dobYear) && this.isValidYear(dobYear)) {
-                this.errors.stack.dobDay = this.errorManifest.validation.dateInFuture;
-            } else if (!this.isNotTooOld(dobDay, dobMonth, dobYear) && this.isValidYear(dobYear)) {
-                this.errors.stack.dobDay = this.errorManifest.validation.tooOld;
-            }
+const dateYearChecker = (day: string, month: string, year: string) => {
+    if (day.trim() !== "" && month.trim() !== "" && year.trim() === "") {
+        throw new Error(dateOfBirthErrorManifest.validation.noYear.summary);
+    }
+    return true;
+};
 
-            if (!this.isValidMonth(dobMonth) && payload.dobMonth !== "") {
-                this.errors.stack.dobMonth = this.errorManifest.validation.invalid;
-            }
-            if (!this.isValidYear(dobYear) && payload.dobYear !== "") {
-                this.errors.stack.dobYear = this.errorManifest.validation.invalid;
-            }
-            if (!this.isValidDay(dobDay, dobMonth, dobYear) && payload.dobDay !== "" && payload.dobMonth !== "" && payload.dobYear !== "") {
-                this.errors.stack.dobDay = this.errorManifest.validation.invalid;
-            }
-
-            if (!Object.keys(this.errors.stack).length) {
-                return Promise.resolve(true);
-            } else {
-                return Promise.reject(this.errors);
-            }
-
-        } catch (error) {
-            this.errors.stack = this.errorManifest.generic.serverError;
-            return Promise.reject(this.errors);
+const validDataChecker = (day: string, month: string, year: string) => {
+    if (day.trim() !== "" && month.trim() !== "" && year.trim() !== "") {
+        if (+month < 1 || +month > 12 || +year < 1000 || +year > 9999 || !isReadDay(+day, +month, +year)) {
+            throw new Error(dateOfBirthErrorManifest.validation.invalid.summary);
+        } else if (!isNotInFuture(+day, +month, +year)) {
+            throw new Error(dateOfBirthErrorManifest.validation.dateInFuture.summary);
+        } else if (!isNotTooYoung(+day, +month, +year)) {
+            throw new Error(dateOfBirthErrorManifest.validation.tooYoung.summary);
+        } else if (!isNotTooOld(+day, +month, +year)) {
+            throw new Error(dateOfBirthErrorManifest.validation.tooOld.summary);
         }
     }
+    return true;
+};
 
-    isValidMonth (month: number):boolean {
-        if (typeof month !== "number") {
-            return false;
-        } else if (month < 1 || month > 12) {
-            return false;
-        }
+const isReadDay = (day: number, month: number, year: number):boolean => {
+    const numbDays = new Date(year, month, 0).getDate();
+    if (day >= 1 && day <= numbDays) {
         return true;
     }
+    return false;
+};
 
-    isValidYear (year: number):boolean {
-        if (typeof year !== "number") {
-            return false;
-        } else if (year < 1000 || year > 9999) {
-            return false;
-        }
-        return true;
+const isNotInFuture = (day: number, month: number, year: number): boolean => {
+    var currentDate = new Date();
+    var inputDate = new Date(year, month - 1, day);
+
+    if (inputDate > currentDate) {
+        return false;
     }
+    return true;
+};
 
-    isValidDay (day: number, month: number, year: number): boolean {
-        var numDays = new Date(year, month, 0).getDate();
-        if (typeof day !== "number") {
-            return false;
-        }
-        if (day < 1 || day > numDays) {
-            return false;
-        }
-        return true;
+const isNotTooYoung = (day: number, month: number, year: number): boolean => {
+    var currentDate = new Date();
+    var inputDate = new Date(year, month - 1, day);
+    var age = currentDate.getFullYear() - inputDate.getFullYear();
+
+    if (currentDate.getMonth() < inputDate.getMonth() || (currentDate.getMonth() === inputDate.getMonth() && currentDate.getDate() < inputDate.getDate())) {
+        age--;
     }
+    return age >= 16;
+};
 
-    isNotInFuture (day: number, month: number, year: number): boolean {
-        var currentDate = new Date();
-        var inputDate = new Date(year, month - 1, day);
+const isNotTooOld = (day: number, month: number, year: number): boolean => {
+    var currentDate = new Date();
+    var inputDate = new Date(year, month - 1, day);
+    var age = currentDate.getFullYear() - inputDate.getFullYear();
 
-        if (inputDate > currentDate) {
-            return false;
-        }
-        return true;
+    if (currentDate.getMonth() > inputDate.getMonth() || (currentDate.getMonth() === inputDate.getMonth() && currentDate.getDate() > inputDate.getDate())) {
+        age++;
     }
-
-    isNotTooYoung (day: number, month: number, year: number): boolean {
-        var currentDate = new Date();
-        var inputDate = new Date(year, month - 1, day);
-        var age = currentDate.getFullYear() - inputDate.getFullYear();
-
-        if (currentDate.getMonth() < inputDate.getMonth() || (currentDate.getMonth() === inputDate.getMonth() && currentDate.getDate() < inputDate.getDate())) {
-            age--;
-        }
-        return age >= 16;
-    }
-
-    isNotTooOld (day: number, month: number, year: number): boolean {
-        var currentDate = new Date();
-        var inputDate = new Date(year, month - 1, day);
-        var age = currentDate.getFullYear() - inputDate.getFullYear();
-
-        if (currentDate.getMonth() > inputDate.getMonth() || (currentDate.getMonth() === inputDate.getMonth() && currentDate.getDate() > inputDate.getDate())) {
-            age++;
-        }
-        return age <= 110;
-    }
-
+    return age <= 110;
 };
