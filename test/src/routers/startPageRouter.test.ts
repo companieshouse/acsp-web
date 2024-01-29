a151ca0a5aeb9545debe8d9b99cfd2559a65238e
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-01-29 15:01:10 +0000

    Making req changes.

--- test/src/routers/startPageRouter.test.ts
+++ test/src/routers/startPageRouter.test.ts
@@ -3,7 +3,7 @@ import supertest from "supertest";
 const router = supertest(app);
 
 describe('Start Page Router', () => {
-   it("should respond with status 200 for GET /start-page", async () => {
-       const response = await supertest(app).get("/start-page");
+   it("should respond with status 200 for GET /", async () => {
+       const response = await supertest(app).get("/");
     });
 });
