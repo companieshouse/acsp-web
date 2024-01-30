4b2b9ebcc35927e1d8f30a69a654985177bbb76f
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-01-30 10:13:36 +0000

    Added error box on last name when no name is entered. Created unit tests for custom validation.

---
+++ test/src/lib/validation/name.test.ts
@@ -0,0 +1,29 @@
+import nameErrorManifest from "../../../../src/lib/utils/error_manifests/name";
+import { firstNameChecker, lastNameChecker, middleNameChecker } from "../../../../src/lib/validation/name";
+
+describe("Missing input validation tests", () => {
+    test("Error if name fields are completely empty", async () => {
+        expect(() => firstNameChecker("", "")).toThrow(new Error(nameErrorManifest.validation.noData.summary));
+    });
+    test("Error if first name field is empty", async () => {
+        expect(() => firstNameChecker("", "Doe")).toThrow(new Error(nameErrorManifest.validation.noFirstName.summary));
+    });
+    test("Error if last name field is empty", async () => {
+        expect(() => lastNameChecker("John", "")).toThrow(new Error(nameErrorManifest.validation.noLastName.summary));
+    });
+    test("No error if first and last name entered", async () => {
+        expect(() => lastNameChecker("John", "Doe")).toBeTruthy();
+    });
+});
+
+describe("Valid data input tests", () => {
+    test("Error if first name field is not valid input", async () => {
+        expect(() => firstNameChecker("J0hn&/", "Doe")).toThrow(new Error(nameErrorManifest.validation.firstName.summary));
+    });
+    test("Error if last name field is not valid input", async () => {
+        expect(() => lastNameChecker("John", "DO&@")).toThrow(new Error(nameErrorManifest.validation.lastName.summary));
+    });
+    test("Error if middle names field is not valid input", async () => {
+        expect(() => middleNameChecker("And?£*")).toThrow(new Error(nameErrorManifest.validation.middleName.summary));
+    });
+});
