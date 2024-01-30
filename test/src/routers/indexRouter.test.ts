dcff476f2aa8f81aaac2a2fe1e1f1ee82831f675
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-01-30 16:46:18 +0000

    Updated view to reflect new design and fixed error where some contries did not display

--- test/src/routers/indexRouter.test.ts
+++ test/src/routers/indexRouter.test.ts
@@ -1,12 +1,12 @@
-import app from "../../../src/app";
 import supertest from "supertest";
+import app from "../../../src/app";
 const router = supertest(app);
 
 describe("GET /", () => {
     it("should return status 200", async () => {
         await router.get("/").expect(200);
     });
-describe("POST /type-of-acsp", () => {
+    describe("POST /type-of-acsp", () => {
         it("should return status 302 after redirect", async () => {
             await router.post("/").expect(302);
         });
