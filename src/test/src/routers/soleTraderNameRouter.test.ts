82fa7405510ef351e721d9c4449cff225302e022
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-02-01 12:12:58 +0000

    added the test

--- src/test/src/routers/soleTraderNameRouter.test.ts
+++ src/test/src/routers/soleTraderNameRouter.test.ts
@@ -9,7 +9,6 @@ describe("GET /sole-trader/name", () => {
 });
 
 describe("POST /sole-trader/name", () => {
-    // Test for correct form details entered, will return 302 after redirecting to the next page.
     it("should redirect with status 302 on successful form submission", async () => {
         const formData = {
             "first-name": "John",
@@ -19,13 +18,10 @@ describe("POST /sole-trader/name", () => {
 
         const response = await router.post("/sole-trader/name").send(formData);
 
-        // Expectations
         expect(response.status).toBe(302); // Expect a redirect status code
         expect(response.header.location).toBe("/sole-trader/date-of-birth");
-        // Add more expectations based on your specific requirements
     });
 
-    // Test for incorrect form details entered, will return 400.
     it("should return status 400 for incorrect data entered", async () => {
         const formData = {
             "first-name": "",
@@ -35,8 +31,6 @@ describe("POST /sole-trader/name", () => {
 
         const response = await router.post("/sole-trader/name").send(formData);
 
-        // Expectations
-        expect(response.status).toBe(400); // Expect a validation failure status code
-        // Add more expectations based on your specific requirements
+        expect(response.status).toBe(400);
     });
 });
