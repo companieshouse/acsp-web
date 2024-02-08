57272d385303ff9968a9f7a603669f96072afa7a
swalke-ch <swalke@companieshouse.gov.uk>
2024-02-08 15:54:59 +0000

    changes for type of business and some refactoring of the code

--- src/main/types/pageURL.ts
+++ src/main/types/pageURL.ts
@@ -2,6 +2,14 @@ const SEPARATOR = "/";
 
 const SOLE_TRADER = "/sole-trader";
 
+export const START = "/"; // Domain name will go here
+
 export const SOLE_TRADER_SECTOR_YOU_WORK_IN = SOLE_TRADER + "/sector-you-work-in";
 
 export const SOLE_TRADER_DATE_OF_BIRTH = SOLE_TRADER + "/date-of-birth";
+
+export const SOLE_TRADER_ROLE = SOLE_TRADER + "/statement-relevant-officer";
+
+export const SOLE_TRADER_TYPE_OFBUSINESS = SOLE_TRADER + "/type-of-business";
+
+export const SOLE_TRADER_OTHER_TYPE_OFBUSINESS = SOLE_TRADER + "/other-type-of-business";
---
+++ src/test/src/controllers/typeOfBusinessController.test.ts
@@ -0,0 +1,26 @@
+import supertest from "supertest";
+import app from "../../../main/app";
+import { SOLE_TRADER_TYPE_OFBUSINESS, START, SOLE_TRADER_OTHER_TYPE_OFBUSINESS } from "../../../main/types/pageURL";
+const router = supertest(app);
+
+describe("GET " + SOLE_TRADER_TYPE_OFBUSINESS, () => {
+    it("should return status 200", async () => {
+        await router.get(SOLE_TRADER_TYPE_OFBUSINESS).expect(200);
+    });
+});
+
+// Test for correct form details entered, will return 302 after redirecting to the next page.
+describe("POST " + SOLE_TRADER_TYPE_OFBUSINESS, () => {
+    it("should return status 302 after redirect", async () => {
+        await router.post(SOLE_TRADER_TYPE_OFBUSINESS).send({ typeOfBusinessRadio: "LIMITED_COMPANY" }).expect(302);
+    });
+    it("should return status 302 after redirect", async () => {
+        await router.post(SOLE_TRADER_TYPE_OFBUSINESS).send({ typeOfBusinessRadio: "OTHER" }).expect(302);
+    });
+});
+// Test for incorrect form details entered, will return 400.
+describe("POST " + SOLE_TRADER_TYPE_OFBUSINESS, () => {
+    it("should return status 400 after incorrect data entered", async () => {
+        await router.post(SOLE_TRADER_TYPE_OFBUSINESS).send({ typeOfBusinessRadio: "" }).expect(400);
+    });
+});
