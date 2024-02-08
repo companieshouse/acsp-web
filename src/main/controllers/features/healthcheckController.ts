b93a82638cce28e0a6efeb045c7319903385e1b8
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-02-08 14:12:13 +0000

    Not required

--- src/main/controllers/features/healthcheckController.ts
+++ src/main/controllers/features/healthcheckController.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response } from "express";
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 export const get = async (req: Request, res: Response) => {
     res.status(200).json({ status: "OK" });
 };
