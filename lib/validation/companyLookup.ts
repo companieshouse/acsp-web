ca95ac60d753aba8af445a9957be9e36e974b3b6
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-02-12 13:01:17 +0000

    -Add Company Number Router

--- lib/validation/companyLookup.ts
+++ lib/validation/companyLookup.ts
@@ -4,10 +4,11 @@ import companyLookupErrorManifest from "../../lib/utils/error_manifests/companyL
 
 const characterPattern:RegExp = /^[a-zA-Z0-9]+$/;
 export const companyNumberValidator = [
-
         body("company_number").trim().notEmpty().withMessage(companyLookupErrorManifest.validation.noData.summary).bail()
                 .matches(characterPattern).withMessage(companyLookupErrorManifest.validation.companyNumber.summary).bail()
                 .isLength({ min: 8, max: 8 }).withMessage(companyLookupErrorManifest.validation.characterLimit.summary)
+
+
 
 
 ];
