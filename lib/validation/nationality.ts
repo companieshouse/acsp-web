ad3a6a2430faa05f6eedf7e8c143834c4dddb42c
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-02-13 08:58:18 +0000

    -change nationality.ts

--- lib/validation/nationality.ts
+++ lib/validation/nationality.ts
@@ -1,20 +1,14 @@
 import { body } from "express-validator";
 import nationalityList from "../nationalityList";
 import nationalityErrorManifest from "../utils/error_manifests/nationality";
-import * as constants from "../../src/main/common/__utils/constants";
-
 
 
 export const nationalityValidator = [
-    // Validation for 'nationalityInput'
-    body("nationality_input_0")
-        .notEmpty().withMessage(nationalityErrorManifest.validation.noNationality.summary)
-        .isIn(nationalityList).withMessage(nationalityErrorManifest.validation.invalid.summary)
-        .isString().withMessage(nationalityErrorManifest.validation.invalid.summary)
-        .isLength({ max: constants.CHARACTER_LIMIT_50 }).withMessage(nationalityErrorManifest.validation.charLimit1stNationality.summary),
+    // Validation for 'nationality_input_0'
+    body("nationality_input_0", nationalityErrorManifest.validation.noNationality.summary).trim().notEmpty().bail().isIn(nationalityList),
 
 
-    // Validation for 'nationalityInputSecond'
+    // Validation for 'nationality_input_1'
     body("nationality_input_1")
         .optional({ nullable: true, checkFalsy: true })
         .isIn(nationalityList).withMessage(nationalityErrorManifest.validation.invalid.summary)
@@ -25,10 +19,19 @@ export const nationalityValidator = [
             }
             return true;
         })
-        .isString().withMessage(nationalityErrorManifest.validation.invalid.summary)
-        .isLength({ max: constants.CHARACTER_LIMIT_49 }).withMessage(nationalityErrorManifest.validation.charLimit2ndNationality.summary),
 
-    // Validation for 'nationalityInputThird'
+        .custom((value, { req }) => {
+            // Check if 'nationality_input_0' is filled, 'nationality_input_1' is empty, and 'nationality_input_2' is filled
+            if (req.body.nationality_input_0 && !value && req.body.nationality_input_2 && value === " ") {
+                throw new Error(nationalityErrorManifest.validation.invalid.summary);
+            }
+            return true;
+        })
+        .isString().withMessage(nationalityErrorManifest.validation.invalid.summary),
+
+
+
+    // Validation for 'nationality_input_2'
     body("nationality_input_2")
         .optional({ nullable: true, checkFalsy: true })
         .isIn(nationalityList).withMessage(nationalityErrorManifest.validation.invalid.summary)
@@ -40,5 +43,7 @@ export const nationalityValidator = [
             return true;
         })
         .isString().withMessage(nationalityErrorManifest.validation.invalid.summary)
-        .isLength({ max: constants.CHARACTER_LIMIT_48 }).withMessage(nationalityErrorManifest.validation.charLimit3rdNationality.summary)
 ];
+
+
+
