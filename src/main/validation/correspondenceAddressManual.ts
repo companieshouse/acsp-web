3db3e5d99430f803a53dae4079b7ecdf65bc2868
sardhani <sardhani@companieshouse.gov.uk>
2024-02-13 13:54:02 +0000

    code refactoring

--- src/main/validation/correspondenceAddressManual.ts
+++ src/main/validation/correspondenceAddressManual.ts
@@ -6,8 +6,6 @@ const addressCountyAndCountryFormat:RegExp = /^[A-Za-z\s]*$/;
 const addressUKPostcodeFormat:RegExp = /^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$/;
 
 export const correspondenceAddressManualValidator = [
-//     body("addressPropertyDetails").trim().notEmpty().withMessage("noPropertyDetails").bail()
-//         .matches(otherAddressDetailsFormat).withMessage("noPropertyDetails").bail()
 
     body("addressPropertyDetails").trim().notEmpty().withMessage("noPropertyDetails").bail()
         .matches(otherAddressDetailsFormat).withMessage("invalidPropertyDetails").bail()
