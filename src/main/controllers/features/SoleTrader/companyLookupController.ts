ae75a6b28323171e591231aabc46fb193a0da1ab
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-02-14 10:34:17 +0000

    -added save functionality data on screen

--- src/main/controllers/features/SoleTrader/companyLookupController.ts
+++ src/main/controllers/features/SoleTrader/companyLookupController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from "express";
 import { validationResult } from "express-validator";
 import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 import * as config from "../../../config";
+import axios from "axios";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     res.render(config.SOLE_TRADER_COMPANY_NUMBER, {
@@ -11,6 +12,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
+    console.log("Data:", req.body.companyNumber);
     try {
         const errorList = validationResult(req);
         if (!errorList.isEmpty()) {
@@ -22,7 +24,19 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
                 previousPage: "/sole-trader/one-login-enter-password"
             });
         } else {
-            res.redirect("/sole-trader/confirm-company");
+            const companyNumber = req.body.companyNumber;
+            const response = await axios.post("/company/{companyNumber}");
+            if (response.status === 200) {
+                res.redirect("/sole-trader/confirm-company");
+            } else if (response.status === 404) {
+            // const pageProperties = getPageProperties({ companyNumber: "Company number does not exist" });
+                res.status(400).render(config.SOLE_TRADER_COMPANY_NUMBER, {
+                // pageProperties: pageProperties,
+                    payload: req.body,
+                    title: "What is the company number?",
+                    previousPage: "/sole-trader/one-login-enter-password"
+                });
+            }
         }
     } catch (error) {
         next(error);
