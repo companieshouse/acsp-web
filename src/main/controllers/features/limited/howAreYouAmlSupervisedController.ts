7ecef3fcec6c586d849ab77c2999d3bf653aa5d7
amanueltedros <atedros@companieshouse.gov.uk>
2024-02-16 14:45:16 +0000

    back url changed

--- src/main/controllers/features/limited/howAreYouAmlSupervisedController.ts
+++ src/main/controllers/features/limited/howAreYouAmlSupervisedController.ts
@@ -3,13 +3,13 @@ import { validationResult } from "express-validator";
 import * as config from "../../../config";
 import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
-import { SOLE_TRADER_SECTOR_YOU_WORK_IN, SOLE_TRADER_DATE_OF_BIRTH, SOLE_TRADER_HOW_ARE_YOU_AML_SUPERVISED, WHAT_IS_YOUR_ROLE } from "../../../types/pageURL";
+import { SOLE_TRADER_SECTOR_YOU_WORK_IN, SOLE_TRADER_HOW_ARE_YOU_AML_SUPERVISED, WHAT_IS_YOUR_ROLE } from "../../../types/pageURL";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     res.render(config.SOLE_TRADER_HOW_ARE_YOU_AML_SUPERVISED, {
-        previousPage: addLangToUrl(SOLE_TRADER_DATE_OF_BIRTH, lang),
+        previousPage: addLangToUrl(WHAT_IS_YOUR_ROLE, lang),
         title: "Which sector do you work in?",
         ...getLocaleInfo(locales, lang),
         currentUrl: SOLE_TRADER_HOW_ARE_YOU_AML_SUPERVISED
