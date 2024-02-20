5c0805200115a06864bd5980d8311f438a92f49a
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-02-20 16:26:16 +0000

    -Add ascpClientService

--- src/main/clients/ASCPServiceClient.ts
+++ src/main/clients/ASCPServiceClient.ts
@@ -4,22 +4,23 @@ import logger from "../../../lib/Logger";
 
 export class ACSPServiceClient {
     // const baseURL = "http://localhost:18642/acsp-api/";
-  private client: AxiosInstance;
+    private client: AxiosInstance;
 
-  constructor (baseURL: string) {
-      this.client = Axios.create({
-          baseURL
+    constructor (baseURL: string) {
+        this.client = Axios.create({
+            baseURL
 
-      });
-  }
+        });
+    }
 
-  public async getCompany (companyNumber: string) {
-      try {
-          const response = await this.client.get(`/company/${companyNumber}`);
-          logger.info(response.data);
-
-      } catch (err:any) {
-          logger.error(err);
-      }
-  }
+    public async getCompany (companyNumber: string): Promise<any> {
+        try {
+            const response = await this.client.get(`/company/${companyNumber}`);
+            logger.info(response.data);
+            return response.data;
+        } catch (err: any) {
+            logger.error(err);
+            throw err;
+        }
+    }
 }
