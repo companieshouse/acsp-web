85c84a098113f3109145b3f9e7064774ba125fc3
sardhani <sardhani@companieshouse.gov.uk>
2024-02-21 09:44:59 +0000

    pipeline fix

--- src/main/controllers/index.ts
+++ src/main/controllers/index.ts
@@ -14,4 +14,4 @@ export * as typeOfBusinessController from "./features/SoleTrader/typeOfBusinessC
 export * as OtherTypeOfBusinessController from "./features/SoleTrader/otherTypeOfBusinessController";
 export * as healthCheckController from "./healthCheckController";
 export * as nameRegisteredWithAmlController from "./features/limited/nameRegisteredWithAmlController";
-export * as businessMustbeAmlRegisteredController from "./features/limited/businessMustbeAmlRegisteredController";
+export * as businessMustbeAmlRegisteredController from "./features/Limited/businessMustbeAmlRegisteredController";
