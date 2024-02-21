749afe8b757c6d0ac86f51fd387f6cee46da8cee
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-02-21 15:00:56 +0000

    Calling Company name from session

--- src/main/controllers/features/limited/companyInactiveController.ts
+++ src/main/controllers/features/limited/companyInactiveController.ts
@@ -2,19 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import * as config from "../../../config";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { LIMITED_WHAT_IS_THE_COMPANY_NUMBER, LIMITED_COMPANY_INACTIVE, START, BASE_URL } from "../../../types/pageURL";
-import { CompanyNameService } from "../../../services/companyNameInactiveService";
-
-const companyNameService = new CompanyNameService();
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
+    req.session.user = req.session.user || {};
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
-    const companyName = companyNameService.getCompanyName();
     res.render(config.LIMITED_COMPANY_INACTIVE, {
         previousPage: addLangToUrl(BASE_URL + LIMITED_WHAT_IS_THE_COMPANY_NUMBER, lang),
         startPage: addLangToUrl(BASE_URL, lang),
         title: "Which name is registered with your AML supervisory body?",
-        companyName,
+        companyName: req.session.user.companyName,
         ...getLocaleInfo(locales, lang),
         currentUrl: BASE_URL + LIMITED_COMPANY_INACTIVE
     });
