1266f1faa391004a81067df7f5d8742b941c2bcf
amanueltedros <atedros@companieshouse.gov.uk>
2024-02-22 15:12:03 +0000

    test added

---
+++ src/test/src/controllers/unincorporatedWhatIsTheBusinessName.test.ts
@@ -0,0 +1,43 @@
+import supertest from "supertest";
+import app from "../../../main/app";
+import { BASE_URL, SOLE_TRADER_SECTOR_YOU_WORK_IN, UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME } from "../../../main/types/pageURL";
+const router = supertest(app);
+
+describe("GET" + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME, () => {
+    it("should return status 200", async () => {
+        await router.get(BASE_URL + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME).expect(200);
+    });
+});
+
+describe("POST" + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME, () => {
+    it("should redirect with status 302 on successful form submission", async () => {
+        const formData = {
+            whatIsTheBusinessName: "Company"
+        };
+
+        const response = await router.post(BASE_URL + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME).send(formData);
+
+        expect(response.status).toBe(302); // Expect a redirect status code
+        expect(response.header.location).toBe(BASE_URL + SOLE_TRADER_SECTOR_YOU_WORK_IN + "?lang=en");
+    });
+
+    it("should return status 400 for incorrect data entered", async () => {
+        const formData = {
+            whatIsTheBusinessName: ""
+        };
+
+        const response = await router.post(BASE_URL + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME).send(formData);
+
+        expect(response.status).toBe(400);
+    });
+
+    it("should return status 400 for incorrect data entered", async () => {
+        const formData = {
+            whatIsTheBusinessName: "Company@@$%^"
+        };
+
+        const response = await router.post(BASE_URL + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME).send(formData);
+
+        expect(response.status).toBe(400);
+    });
+});
