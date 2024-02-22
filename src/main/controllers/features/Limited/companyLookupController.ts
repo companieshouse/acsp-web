710bdda1d98070dd7315374ae8a875de397d0be4
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-02-22 09:34:49 +0000

    translation

--- src/main/controllers/features/Limited/companyLookupController.ts
+++ src/main/controllers/features/Limited/companyLookupController.ts
@@ -3,40 +3,55 @@ import { validationResult } from "express-validator";
 import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 import * as config from "../../../config";
 import { ACSPServiceClient } from "../../../clients/ASCPServiceClient";
-import { BASE_URL, LIMITED_CONFIRM_COMPANY, LIMITED_ONE_LOGIN_PASSWORD } from "../../../types/pageURL";
+import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
+import { BASE_URL, LIMITED_CONFIRM_COMPANY, LIMITED_ONE_LOGIN_PASSWORD,LIMITED_COMPANY_NUMBER } from "../../../types/pageURL";
 
 const acspServiceClientOne = new ACSPServiceClient("http://localhost:18642/acsp-api");
 
-export const get = async (req: Request, res: Response) => {
+export const get = async (req: Request, res: Response,next: NextFunction) => {
+    const lang = selectLang(req.query.lang);
+    const locales = getLocalesService();
     res.render(config.LIMITED_COMPANY_NUMBER, {
+        previousPage: addLangToUrl(BASE_URL + LIMITED_ONE_LOGIN_PASSWORD, lang),
         title: "What is the company number?",
-        previousPage: BASE_URL + LIMITED_ONE_LOGIN_PASSWORD
+        ...getLocaleInfo(locales, lang),
+        currentUrl: BASE_URL + LIMITED_COMPANY_NUMBER
     });
 };
 
-export const post = async (req: Request, res: Response) => {
+export const post = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            const pageProperties = getPageProperties(formatValidationError(errors.array()));
+        //const errors = validationResult(req);
+        const lang = selectLang(req.query.lang);
+        const locales = getLocalesService();
+        const errorList = validationResult(req);
+        if (!errorList.isEmpty()) {
+            const pageProperties = getPageProperties(formatValidationError(errorList.array()));
             res.status(400).render(config.LIMITED_COMPANY_NUMBER, {
-                pageProperties,
+                previousPage:addLangToUrl(BASE_URL + LIMITED_ONE_LOGIN_PASSWORD, lang),
                 payload: req.body,
                 title: "What is the company number?",
-                previousPage: BASE_URL + LIMITED_ONE_LOGIN_PASSWORD
+                ...getLocaleInfo(locales, lang),
+                currentUrl: BASE_URL + LIMITED_COMPANY_NUMBER,
+                pageProperties:pageProperties
+                
+                
             });
         } else {
             const { companyNumber } = req.body;
             await acspServiceClientOne.getCompany(companyNumber);
             res.redirect(BASE_URL + LIMITED_CONFIRM_COMPANY);
+            const nextPageUrl = addLangToUrl(BASE_URL + LIMITED_CONFIRM_COMPANY, lang);
+            res.redirect(nextPageUrl);
         }
     } catch (error) {
-        res.status(404).render(config.LIMITED_COMPANY_NUMBER, {
-            // pageProperties: getPageProperties({ companyNumber: 'An error occurred while checking company existence' }),
+        next(error);
+        /*res.status(404).render(config.LIMITED_COMPANY_NUMBER, {
+
             payload: req.body,
             title: "What is the company number?",
             previousPage: BASE_URL + LIMITED_ONE_LOGIN_PASSWORD
-        });
+        });*/
     }
 };
 
--- src/main/validation/companyLookup.ts
+++ src/main/validation/companyLookup.ts
@@ -5,9 +5,9 @@ import { ACSPServiceClient } from "../../main/clients/ASCPServiceClient";
 
 const characterPattern:RegExp = /^[a-zA-Z0-9]+$/;
 export const companyNumberValidator = [
-    body("companyNumber").trim().notEmpty().withMessage(companyLookupErrorManifest.validation.noData.summary).bail()
-        .matches(characterPattern).withMessage(companyLookupErrorManifest.validation.companyNumber.summary).bail()
-        .isLength({ min: 8, max: 8 }).withMessage(companyLookupErrorManifest.validation.characterLimit.summary).bail()
+    body("companyNumber").trim().notEmpty().withMessage("invalidcompanyNumberNoData").bail()
+        .matches(characterPattern).withMessage("invalidcompanyNumberPattern").bail()
+        .isLength({ min: 8, max: 8 }).withMessage("invalidcompanyNumberPattern").bail()
         .custom(async (value: string) => {
             // Check if company number exists
             const acspServiceClient = new ACSPServiceClient("http://localhost:18642/acsp-api");
