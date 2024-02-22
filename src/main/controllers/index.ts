021544ff3b3ca4c00b5974e65a1a990239a047fc
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-02-22 17:01:08 +0000

    Saving Company details into session.

--- src/main/controllers/index.ts
+++ src/main/controllers/index.ts
@@ -13,8 +13,8 @@ export * as soleTraderNationalityController from "./features/SoleTrader/soleTrad
 export * as typeOfBusinessController from "./features/SoleTrader/typeOfBusinessController";
 export * as OtherTypeOfBusinessController from "./features/SoleTrader/otherTypeOfBusinessController";
 export * as healthCheckController from "./healthCheckController";
-export * as nameRegisteredWithAmlController from "./features/Limited/nameRegisteredWithAmlController";
-export * as businessMustbeAmlRegisteredController from "./features/Limited/businessMustbeAmlRegisteredController";
-export * as companyLookupController from "./features/Limited/companyLookupController";
-export * as companyInactiveController from "./features/Limited/companyInactiveController";
-export * as isThisYourCompanyController from "./features/Limited/isThisYourCompanyController";
+export * as nameRegisteredWithAmlController from "./features/limited/nameRegisteredWithAmlController";
+export * as businessMustbeAmlRegisteredController from "./features/limited/businessMustbeAmlRegisteredController";
+export * as companyLookupController from "./features/limited/companyLookupController";
+export * as companyInactiveController from "./features/limited/companyInactiveController";
+export * as isThisYourCompanyController from "./features/limited/isThisYourCompanyController";
