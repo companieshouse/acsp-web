57ff6257e2800dc01036ec3de5006e9edcc7d893
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-02-23 18:57:59 +0000

    -adjust test

--- src/test/src/controllers/companyLookupControllerRouter.test.ts
+++ src/test/src/controllers/companyLookupControllerRouter.test.ts
@@ -1,16 +1,16 @@
 import supertest from "supertest";
-import request from "supertest";
 import app from "../../../main/app";
-const router = supertest(app);
 import { BASE_URL, LIMITED_COMPANY_NUMBER } from "../../../main/types/pageURL";
+
+const router = supertest(app);
 
 describe("CompanyLookupController", () => {
     test("GET" + LIMITED_COMPANY_NUMBER, async () => {
-        const res = await request(app).get(BASE_URL + LIMITED_COMPANY_NUMBER);
+        const res = await router.get(BASE_URL + LIMITED_COMPANY_NUMBER);
         expect(res.status).toBe(200);// render company number page
 
     });
-    });
+});
 
 describe("POST" + LIMITED_COMPANY_NUMBER, () => {
     it("should return status 200", async () => {
@@ -45,10 +45,3 @@ describe("POST" + LIMITED_COMPANY_NUMBER, () => {
             .send({ companyNumber: "NI5981260987654321" }).expect(400);
     });
 });
-
-test("POST" + LIMITED_COMPANY_NUMBER, async () => {
-    const res = await request(app)
-        .post(BASE_URL + LIMITED_COMPANY_NUMBER)
-        .send({ companyNumber: "NI038379" });
-
-    expect(res.status).toBe(302); });// Redirect status code - valid company number
