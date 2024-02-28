7c16ff1c3541949947952bcb9e9d26dac4a17b52
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-02-28 14:54:17 +0000

    Correcting all the pages title.

--- src/main/controllers/features/SoleTrader/soleTraderCorrespodanceAddressDetailsController.ts
+++ src/main/controllers/features/SoleTrader/soleTraderCorrespodanceAddressDetailsController.ts
@@ -10,7 +10,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     res.render(config.SOLE_TRADER_CORRESPONDENCE_ADDRESS_LIST, {
-        title: "Select your address?",
+        title: "Select your address",
         ...getLocaleInfo(locales, lang),
         currentUrl: BASE_URL + SOLE_TRADER_AUTO_LOOKUP_ADDRESS_LIST,
         previousPage: addLangToUrl(BASE_URL + SOLE_TRADER_AUTO_LOOKUP_ADDRESS, lang),
