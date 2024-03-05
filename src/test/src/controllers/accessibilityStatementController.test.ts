1aff5568152469febad6f1e8c78ff512e36e4f20
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-03-05 09:50:18 +0000

    Updating test

--- src/test/src/controllers/accessibilityStatementController.test.ts
+++ src/test/src/controllers/accessibilityStatementController.test.ts
@@ -1,17 +1,18 @@
 import supertest from "supertest";
 import app from "../../../main/app";
-import { ACCESSIBILITY_STATEMENT } from "../../../main/types/pageURL";
+import { BASE_URL, ACCESSIBILITY_STATEMENT } from "../../../main/types/pageURL";
 
 const router = supertest(app);
 
 describe("GET" + ACCESSIBILITY_STATEMENT, () => {
     it("should return status 200", async () => {
-        const response = await router.get(ACCESSIBILITY_STATEMENT);
+        const response = await router.get(BASE_URL + ACCESSIBILITY_STATEMENT);
         expect(response.status).toBe(200);
     });
 
     it("should render the accessibility statement page", async () => {
-        const response = await router.get(ACCESSIBILITY_STATEMENT);
+        const response = await router.get(BASE_URL + ACCESSIBILITY_STATEMENT);
         expect(response.text).toContain("Accessibility statement for the Companies House service");
     });
 });
+ 
