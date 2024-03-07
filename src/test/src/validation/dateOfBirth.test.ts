33a999d66278db8c045302cec31431b5a6041725
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-03-07 14:49:21 +0000

    Tests

--- src/test/src/validation/dateOfBirth.test.ts
+++ src/test/src/validation/dateOfBirth.test.ts
@@ -1,27 +1,26 @@
-import dateOfBirthErrorManifest from "../../../../lib/utils/error_manifests/dateOfBirth";
-import { dateDayChecker, dateMonthChecker, dateYearChecker, validDataChecker } from "../../../../lib/validation/dateOfBirth";
+import { dateDayChecker, dateMonthChecker, dateYearChecker, validDataChecker } from "../../../main/validation/dateOfBirth";
 
 describe("Missing input validation tests", () => {
     test("Error if date field is completely empty", async () => {
-        expect(() => dateDayChecker("", "", "")).toThrow(new Error(dateOfBirthErrorManifest.validation.noData.summary));
+        expect(() => dateDayChecker("", "", "")).toThrow(new Error("noData"));
     });
     test("Error if day and month fields are empty", async () => {
-        expect(() => dateDayChecker("", "", "1999")).toThrow(new Error(dateOfBirthErrorManifest.validation.noDayMonth.summary));
+        expect(() => dateDayChecker("", "", "1999")).toThrow(new Error("noDayMonth"));
     });
     test("Error if day and year fields are empty", async () => {
-        expect(() => dateDayChecker("", "02", "")).toThrow(new Error(dateOfBirthErrorManifest.validation.noDayYear.summary));
+        expect(() => dateDayChecker("", "02", "")).toThrow(new Error("noDayYear"));
     });
     test("Error if month and year fields are empty", async () => {
-        expect(() => dateMonthChecker("11", "", "")).toThrow(new Error(dateOfBirthErrorManifest.validation.noMonthYear.summary));
+        expect(() => dateMonthChecker("11", "", "")).toThrow(new Error("noMonthYear"));
     });
     test("Error if day field is empty", async () => {
-        expect(() => dateDayChecker("", "02", "1999")).toThrow(new Error(dateOfBirthErrorManifest.validation.noDay.summary));
+        expect(() => dateDayChecker("", "02", "1999")).toThrow(new Error("noDay"));
     });
     test("Error if Month field is empty", async () => {
-        expect(() => dateMonthChecker("11", "", "1999")).toThrow(new Error(dateOfBirthErrorManifest.validation.noMonth.summary));
+        expect(() => dateMonthChecker("11", "", "1999")).toThrow(new Error("noMonth"));
     });
     test("Error if Year field is empty", async () => {
-        expect(() => dateYearChecker("11", "02", "")).toThrow(new Error(dateOfBirthErrorManifest.validation.noYear.summary));
+        expect(() => dateYearChecker("11", "02", "")).toThrow(new Error("noYear"));
     });
 
     test("No error if all fields are input", async () => {
@@ -31,27 +30,27 @@ describe("Missing input validation tests", () => {
 
 describe("Valid data input tests", () => {
     test("Error if month is greater than 12", async () => {
-        expect(() => validDataChecker("11", "13", "1999")).toThrow(new Error(dateOfBirthErrorManifest.validation.invalid.summary));
+        expect(() => validDataChecker("11", "13", "1999")).toThrow(new Error("invalid"));
     });
     test("Error if month is less than 1", async () => {
-        expect(() => validDataChecker("11", "0", "1999")).toThrow(new Error(dateOfBirthErrorManifest.validation.invalid.summary));
+        expect(() => validDataChecker("11", "0", "1999")).toThrow(new Error("invalid"));
     });
     test("Error if year is greater than 9999", async () => {
-        expect(() => validDataChecker("11", "13", "99999")).toThrow(new Error(dateOfBirthErrorManifest.validation.invalid.summary));
+        expect(() => validDataChecker("11", "13", "99999")).toThrow(new Error("validation"));
     });
     test("Error if year is less than 1000", async () => {
-        expect(() => validDataChecker("11", "13", "999")).toThrow(new Error(dateOfBirthErrorManifest.validation.invalid.summary));
+        expect(() => validDataChecker("11", "13", "999")).toThrow(new Error("invalid"));
     });
     test("Error if day is not valid", async () => {
-        expect(() => validDataChecker("30", "02", "1999")).toThrow(new Error(dateOfBirthErrorManifest.validation.invalid.summary));
+        expect(() => validDataChecker("30", "02", "1999")).toThrow(new Error("invalid"));
     });
     test("Error if date given is in the future", async () => {
-        expect(() => validDataChecker("30", "11", "3490")).toThrow(new Error(dateOfBirthErrorManifest.validation.dateInFuture.summary));
+        expect(() => validDataChecker("30", "11", "3490")).toThrow(new Error("dateInFuture"));
     });
     test("Error if date given is more than 110 years ago", async () => {
-        expect(() => validDataChecker("30", "11", "1490")).toThrow(new Error(dateOfBirthErrorManifest.validation.tooOld.summary));
+        expect(() => validDataChecker("30", "11", "1490")).toThrow(new Error("tooOld"));
     });
     test("Error if date given is less than 16 ago", async () => {
-        expect(() => validDataChecker("29", "01", "2024")).toThrow(new Error(dateOfBirthErrorManifest.validation.tooYoung.summary));
+        expect(() => validDataChecker("29", "01", "2024")).toThrow(new Error("tooYoung"));
     });
 });
