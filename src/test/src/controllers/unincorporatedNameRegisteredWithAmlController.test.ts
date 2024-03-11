06bc00a25c8480d2d7366de7c47412973207e4ed
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-03-11 09:48:46 +0000

    Fixing the test

--- src/test/src/controllers/unincorporatedNameRegisteredWithAmlController.test.ts
+++ src/test/src/controllers/unincorporatedNameRegisteredWithAmlController.test.ts
@@ -7,10 +7,6 @@ import { UNINCORPORATED_NAME_REGISTERED_WITH_AML, BASE_URL } from "../../../main
 jest.mock("@companieshouse/api-sdk-node");
 const router = supertest(app);
 
-beforeEach(() => {
-    jest.resetAllMocks();
-});
-
 describe("GET" + UNINCORPORATED_NAME_REGISTERED_WITH_AML, () => {
     it("should return status 200", async () => {
         await router.get(BASE_URL + UNINCORPORATED_NAME_REGISTERED_WITH_AML).expect(200);
@@ -23,7 +19,10 @@ describe("POST" + UNINCORPORATED_NAME_REGISTERED_WITH_AML, () => {
     it("should return status 302 after redirect", async () => {
         await router.post(BASE_URL + UNINCORPORATED_NAME_REGISTERED_WITH_AML).send({ nameRegisteredWithAml: "YOUR_NAME" }).expect(302);
     });
+});
 
+// Test for no radio btn value selected, will return 400.
+describe("POST" + UNINCORPORATED_NAME_REGISTERED_WITH_AML, () => {
     it("should return status 400 after incorrect data entered", async () => {
     // Add this line to include middleware checks for the 400 case
         await router.post(BASE_URL + UNINCORPORATED_NAME_REGISTERED_WITH_AML).send({ nameRegisteredWithAml: "NAME_OF_THE_BUSINESS" }).expect(302);
