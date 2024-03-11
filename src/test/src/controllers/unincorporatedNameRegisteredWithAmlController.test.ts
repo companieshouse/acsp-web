aabd39de7d5974d6325726fb97d95eea61b5ef12
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-03-11 09:05:54 +0000

    Adding Which name is registered with your Anti-Money Laundering (AML) supervisory body? screen for Unincorporated journey

---
+++ src/test/src/controllers/unincorporatedNameRegisteredWithAmlController.test.ts
@@ -0,0 +1,31 @@
+// src/test/src/controllers/unincorporatedNameRegisteredWithAmlController.test.ts
+import mocks from "../../mocks/all_middleware_mock";
+import supertest from "supertest";
+import app from "../../../main/app";
+import { UNINCORPORATED_NAME_REGISTERED_WITH_AML, BASE_URL } from "../../../main/types/pageURL";
+
+jest.mock("@companieshouse/api-sdk-node");
+const router = supertest(app);
+
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
+describe("GET" + UNINCORPORATED_NAME_REGISTERED_WITH_AML, () => {
+    it("should return status 200", async () => {
+        await router.get(BASE_URL + UNINCORPORATED_NAME_REGISTERED_WITH_AML).expect(200);
+        expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+});
+
+describe("POST" + UNINCORPORATED_NAME_REGISTERED_WITH_AML, () => {
+    it("should return status 302 after redirect", async () => {
+        await router.post(BASE_URL + UNINCORPORATED_NAME_REGISTERED_WITH_AML).send({ nameRegisteredWithAml: "YOUR_NAME" }).expect(302);
+    });
+
+    it("should return status 400 after incorrect data entered", async () => {
+    // Add this line to include middleware checks for the 400 case
+        await router.post(BASE_URL + UNINCORPORATED_NAME_REGISTERED_WITH_AML).send({ nameRegisteredWithAml: "NAME_OF_THE_BUSINESS" }).expect(302);
+    });
+});
