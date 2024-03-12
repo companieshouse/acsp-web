91511d3952b049d5a7c487b365c4b35802e7f69f
amanueltedros <atedros@companieshouse.gov.uk>
2024-03-12 14:44:02 +0000

    prevoius page changed

--- src/main/controllers/features/SoleTrader/soleTraderWhatIsTheBusinessNameController.ts
+++ src/main/controllers/features/SoleTrader/soleTraderWhatIsTheBusinessNameController.ts
@@ -3,13 +3,13 @@ import { validationResult } from "express-validator";
 import * as config from "../../../config";
 import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
-import { BASE_URL, TYPE_OF_BUSINESS, SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME, SOLE_TRADER_SECTOR_YOU_WORK_IN, SOLE_TRADER_WHERE_DO_YOU_LIVE } from "../../../types/pageURL";
+import { BASE_URL, SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME, SOLE_TRADER_SECTOR_YOU_WORK_IN, SOLE_TRADER_WHERE_DO_YOU_LIVE } from "../../../types/pageURL";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     res.render(config.SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME, {
-        previousPage: addLangToUrl(BASE_URL + TYPE_OF_BUSINESS, lang),
+        previousPage: addLangToUrl(BASE_URL + SOLE_TRADER_WHERE_DO_YOU_LIVE, lang),
         title: "What is the business name?",
         ...getLocaleInfo(locales, lang),
         currentUrl: BASE_URL + SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME
@@ -24,7 +24,7 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
         if (!errorList.isEmpty()) {
             const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
             res.status(400).render(config.SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME, {
-                previousPage: addLangToUrl(BASE_URL + TYPE_OF_BUSINESS, lang),
+                previousPage: addLangToUrl(BASE_URL + SOLE_TRADER_WHERE_DO_YOU_LIVE, lang),
                 title: "What is the business name?",
                 payload: req.body,
                 ...getLocaleInfo(locales, lang),
