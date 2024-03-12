1a31dbf3061bf7325a5968961fe2a1dbed412b05
akachwala-ch <akachwala@companieshouse.gov.uk>
2024-03-12 11:30:38 +0000

    update TF module ver and trigger log level

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -5,15 +5,5 @@
 PORT=3000
 
 export NODE_PORT=${PORT}
-for entry in /opt/*
-do
-  echo "$entry"
-done
 
-for entry in /opt/assets
-do
-  echo "$entry"
-done
-
-ls -R /opt/assets
 exec node /opt/dist/src/main/server.js -- ${PORT}
