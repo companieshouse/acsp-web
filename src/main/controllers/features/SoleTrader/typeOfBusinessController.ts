46b35fb4ca4947bfa799dce44c24e1bdd3624866
sardhani <sardhani@companieshouse.gov.uk>
2024-03-12 11:36:15 +0000

    what is your role

--- src/main/controllers/features/SoleTrader/typeOfBusinessController.ts
+++ src/main/controllers/features/SoleTrader/typeOfBusinessController.ts
@@ -5,8 +5,10 @@ import { FormattedValidationErrors, formatValidationError } from "../../../valid
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { TYPE_OF_BUSINESS, START, OTHER_TYPE_OFBUSINESS, SOLE_TRADER_WHAT_IS_YOUR_ROLE, BASE_URL, LIMITED_WHAT_IS_THE_COMPANY_NUMBER, UNINCORPORATED_NAME_REGISTERED_WITH_AML, HOME_URL } from "../../../types/pageURL";
 import { TypeOfBusinessService } from "../../..//services/typeOfBusinessService";
-import { SUBMISSION_ID, TRANSACTION_CREATE_ERROR } from "../../../common/__utils/constants";
+import { SUBMISSION_ID, TRANSACTION_CREATE_ERROR, ACSP_TYPE } from "../../../common/__utils/constants";
 import logger from "../../../../../lib/Logger";
+import { saveDataInSession } from "../../../common/__utils/sessionHelper";
+import { Session } from "@companieshouse/node-session-handler";
 import { saveDataInSession, getSessionValue } from "../../../common/__utils/sessionHelper";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
@@ -52,7 +54,12 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
                 ...pageProperties
             });
         } else {
-
+            const session: Session = req.session as any as Session;
+            const acspType = req.body.typeOfBusinessRadio;
+            console.log(acspType);
+            if (session) {
+                session.setExtraData(ACSP_TYPE, acspType);
+            }
             switch (selectedOption) {
             case "LIMITED_COMPANY":
             case "LIMITED_PARTNERSHIP":
--- src/main/controllers/features/unincorporated/unincorporatedWhatIsYourRoleController.ts
+++ src/main/controllers/features/unincorporated/unincorporatedWhatIsYourRoleController.ts
@@ -1,23 +1,27 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import * as config from "../../../config";
-import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
-import { BASE_URL, STOP_NOT_RELEVANT_OFFICER, SOLE_TRADER_NAME_REGISTERED_WITH_AML, SOLE_TRADER_WHAT_IS_YOUR_ROLE } from "../../../types/pageURL";
-import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { validationResult } from "express-validator";
+import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
+import { BASE_URL, UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME, UNINCORPORATED_WHAT_IS_YOUR_ROLE, UNINCORPORATED_WHICH_SECTOR, STOP_NOT_RELEVANT_OFFICER } from "../../../types/pageURL";
+import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { Session } from "@companieshouse/node-session-handler";
-import { ACSP_TYPE } from "../../../common/__utils/constants";
+import { ACSP_TYPE, UNINCORPORATED_BUSINESS_NAME } from "../../../common/__utils/constants";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
+    console.log("reached");
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const session: Session = req.session as any as Session;
     const acspType = session?.getExtraData(ACSP_TYPE)!;
-    console.log("soletraderAcspType");
+    const unincorporatedBusinessName = session?.getExtraData(UNINCORPORATED_BUSINESS_NAME)!;
+    console.log("unincorporatedAcspType");
     res.render(config.WHAT_IS_YOUR_ROLE, {
-        title: "What is your role?",
+        title: "What is your role in the business?",
         ...getLocaleInfo(locales, lang),
-        currentUrl: BASE_URL + SOLE_TRADER_WHAT_IS_YOUR_ROLE,
-        acspType: acspType
+        previousPage: addLangToUrl(BASE_URL + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME, lang),
+        currentUrl: BASE_URL + UNINCORPORATED_WHAT_IS_YOUR_ROLE,
+        acspType: acspType,
+        unincorporatedBusinessName: unincorporatedBusinessName
     });
 };
 
@@ -26,26 +30,22 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
         const lang = selectLang(req.query.lang);
         const locales = getLocalesService();
         const errorList = validationResult(req);
-        const selectedRole = req.body.WhatIsYourRole;
-
+        console.log(errorList);
         if (!errorList.isEmpty()) {
             const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
             res.status(400).render(config.WHAT_IS_YOUR_ROLE, {
                 title: "What is your role in the business?",
                 ...getLocaleInfo(locales, lang),
-                currentUrl: BASE_URL + SOLE_TRADER_WHAT_IS_YOUR_ROLE,
-                ...pageProperties
+                previousPage: addLangToUrl(BASE_URL + UNINCORPORATED_WHAT_IS_THE_BUSINESS_NAME, lang),
+                currentUrl: BASE_URL + UNINCORPORATED_WHAT_IS_YOUR_ROLE,
+                pageProperties: pageProperties,
+                payload: req.body
             });
         } else {
-
-            switch (selectedRole) {
-            case "SOLE_TRADER":
-                res.redirect(addLangToUrl(BASE_URL + SOLE_TRADER_NAME_REGISTERED_WITH_AML, lang));
-                break;
-            case "SOMEONE_ELSE":
+            if (req.body.WhatIsYourRole === "SOMEONE_ELSE") {
                 res.redirect(addLangToUrl(BASE_URL + STOP_NOT_RELEVANT_OFFICER, lang));
-                break;
-
+            } else {
+                res.redirect(BASE_URL + UNINCORPORATED_WHICH_SECTOR);
             }
         }
     } catch (error) {
