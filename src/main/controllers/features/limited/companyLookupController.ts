05e1a3a2628ce0c63d861dcf19486e009b2fe21a
sampankumar <skumar@companieshouse.gov.uk>
2024-03-15 14:32:26 +0000

    Showing backend error on the field

--- src/main/controllers/features/limited/companyLookupController.ts
+++ src/main/controllers/features/limited/companyLookupController.ts
@@ -1,13 +1,12 @@
-import { NextFunction, Request, Response, Router } from "express";
-import { body, validationResult } from "express-validator";
-import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
-import { CompanyDetailsService } from "../../../services/company-details/companyDetailsService";
-import * as config from "../../../config";
-import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
-import { BASE_URL, LIMITED_IS_THIS_YOUR_COMPANY, LIMITED_ONE_LOGIN_PASSWORD, LIMITED_WHAT_IS_THE_COMPANY_NUMBER } from "../../../types/pageURL";
-import { CompanyLookupService } from "../../../services/companyLookupService";
 import { Session } from "@companieshouse/node-session-handler";
-import logger from "../../../../../lib/Logger";
+import { NextFunction, Request, Response } from "express";
+import { ValidationError, validationResult } from "express-validator";
+import * as config from "../../../config";
+import { CompanyDetailsService } from "../../../services/company-details/companyDetailsService";
+import { CompanyLookupService } from "../../../services/companyLookupService";
+import { BASE_URL, LIMITED_IS_THIS_YOUR_COMPANY, LIMITED_ONE_LOGIN_PASSWORD, LIMITED_WHAT_IS_THE_COMPANY_NUMBER } from "../../../types/pageURL";
+import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../../../utils/localise";
+import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 
 const companyDetailsService = new CompanyDetailsService();
 
@@ -30,7 +29,6 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
         const errorList = validationResult(req);
         const session: Session = req.session as any as Session;
         if (!errorList.isEmpty()) {
-            logger.info("------------in if------------- " + JSON.stringify(errorList.array()));
             const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
             res.status(400).render(config.LIMITED_COMPANY_NUMBER, {
                 previousPage: addLangToUrl(BASE_URL + LIMITED_ONE_LOGIN_PASSWORD, lang),
@@ -42,51 +40,26 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
 
             });
         } else {
-            logger.info("------------in else-------------");
             const { companyNumber } = req.body;
             const companyLookupService = new CompanyLookupService();
-            try {
-                getCompanyDetails(companyLookupService, session, companyNumber, req).then(
-                    (result) => {
-                        if (!res.headersSent) {
-                            const nextPageUrl = addLangToUrl(BASE_URL + LIMITED_IS_THIS_YOUR_COMPANY, lang);
-                            res.redirect(nextPageUrl);
-                        }
-                    }).catch(() => {
-                    logger.info("------------in error-------------");
-                    const errors = { errorList: [] } as any;
-                    const errorMessage = "Enter a valid company number";
-                    // errors.errorList[] relates to the linked error messages at the top of the page
-                    errors.errorList.push({ href: "#companyNumber", text: errorMessage });
-                    // errors[] relates to the highlighed fields and the message just above those fields
-                    errors.companyNumber = { text: errorMessage };
-                    res.status(400).render(config.LIMITED_COMPANY_NUMBER, {
-                        previousPage: addLangToUrl(BASE_URL + LIMITED_ONE_LOGIN_PASSWORD, lang),
-                        payload: req.body,
-                        title: "What is the company number?",
-                        ...getLocaleInfo(locales, lang),
-                        currentUrl: BASE_URL + LIMITED_WHAT_IS_THE_COMPANY_NUMBER,
-                        pageProperties: errors.companyNumber
-                    });
-                });
-            } catch (error) {
-                logger.info("------------in catch-------------");
-                const errors = { errorList: [] } as any;
-                const errorMessage = "Enter a valid company number";
-                // errors.errorList[] relates to the linked error messages at the top of the page
-                errors.errorList.push({ href: "#companyNumber", text: errorMessage });
-                // errors[] relates to the highlighed fields and the message just above those fields
-                errors.companyNumber = { text: errorMessage };
+            getCompanyDetails(companyLookupService, session, companyNumber, req).then(
+                (result) => {
+                    if (!res.headersSent) {
+                        const nextPageUrl = addLangToUrl(BASE_URL + LIMITED_IS_THIS_YOUR_COMPANY, lang);
+                        res.redirect(nextPageUrl);
+                    }
+                }).catch((error) => {
+                const pageProperties = getPageProperties(formatValidationError(error, lang));
                 res.status(400).render(config.LIMITED_COMPANY_NUMBER, {
                     previousPage: addLangToUrl(BASE_URL + LIMITED_ONE_LOGIN_PASSWORD, lang),
                     payload: req.body,
                     title: "What is the company number?",
                     ...getLocaleInfo(locales, lang),
                     currentUrl: BASE_URL + LIMITED_WHAT_IS_THE_COMPANY_NUMBER,
-                    pageProperties: errors.companyNumber
-
+                    pageProperties: pageProperties
                 });
-            }
+            });
+
         }
     } catch (error : any) {
         if (error.response) {
@@ -109,16 +82,17 @@ const getPageProperties = (errors?: FormattedValidationErrors) => ({
 });
 
 async function getCompanyDetails (companyLookupService: CompanyLookupService, session: Session, companyNumber: string, req: Request) {
-    logger.info("------------going to get company details-------------");
     await companyLookupService.getCompany(session, companyNumber).then(
         (companyDetails) => {
-            logger.info("------------got company details-------------");
-            companyDetailsService.saveToSession(req, companyDetails); // Prints "Success: The promise has successfully resolved!"
-        },
-        (error) => {
-            logger.info("------------error in getting company details-------------");
-            Promise.reject(error);
-        }
+            companyDetailsService.saveToSession(req, companyDetails);
+        }).catch((companyNumber) => {
+        const validationError : ValidationError[] = [{
+            value: companyNumber,
+            msg: "companyNumberDontExsits",
+            param: "companyNumber",
+            location: "body"
+        }]; ;
+        throw validationError;
+    });
 
-    );
 }
