0108afc6a92301c8f96e741efe7806ae8ca465a6
sardhani <sardhani@companieshouse.gov.uk>
2024-03-20 09:48:49 +0000

    pageurls - fix context path

--- src/test/src/controllers/indexController.test.ts
+++ src/test/src/controllers/indexController.test.ts
@@ -4,10 +4,10 @@ import { BASE_URL } from "../../../main/types/pageURL";
 const router = supertest(app);
 
 describe("Home Page tests -", () => {
-
-    describe("GET /register-acsp/home", () => {
+"GET " + BASE_URL +
+    describe("GET /register-as-companies-house-authorised-agent/home", () => {
         it("should return status 200", async () => {
-            await router.get("/register-acsp/home");
+            await router.get("/register-as-companies-house-authorised-agent/home");
             expect(200);
         });
     });
