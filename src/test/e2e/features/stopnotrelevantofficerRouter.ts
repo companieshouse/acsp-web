f9e5297b68d73b627cf05549cdd35a41e5a3fc2d
sardhani <sardhani@companieshouse.gov.uk>
2024-03-20 10:39:17 +0000

    pageurls - fix context path

--- src/test/e2e/features/stopnotrelevantofficerRouter.ts
+++ src/test/e2e/features/stopnotrelevantofficerRouter.ts
@@ -1,10 +1,11 @@
 import assert from "assert";
 import supertest from "supertest";
 import app from "../../../main/app";
+import { BASE_URL, STOP_NOT_RELEVANT_OFFICER } from "../../../main/types/pageURL";
 
 describe("E2E Test", () => {
     it("should render stop-not-relevant-officer page", async () => {
-        const response = await supertest(app).get("/register-as-companies-house-authorised-agent/sole-trader/stop-not-relevant-officer");
+        const response = await supertest(app).get(BASE_URL + STOP_NOT_RELEVANT_OFFICER);
         assert(response.text.includes("Stop Not Relevant Officer"));
     });
 
--- src/test/src/controllers/indexController.test.ts
+++ src/test/src/controllers/indexController.test.ts
@@ -1,13 +1,12 @@
 import supertest from "supertest";
 import app from "../../../main/app";
-import { BASE_URL } from "../../../main/types/pageURL";
+import { BASE_URL, HOME_URL } from "../../../main/types/pageURL";
 const router = supertest(app);
 
 describe("Home Page tests -", () => {
-"GET " + BASE_URL +
-    describe("GET /register-as-companies-house-authorised-agent/home", () => {
+    describe("GET " + HOME_URL, () => {
         it("should return status 200", async () => {
-            await router.get("/register-as-companies-house-authorised-agent/home");
+            await router.get(BASE_URL + HOME_URL);
             expect(200);
         });
     });
