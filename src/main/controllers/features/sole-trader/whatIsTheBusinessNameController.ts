ed8d71bbd08cc3e1d4571a476ef9c03864ec5b96
amanueltedros <atedros@companieshouse.gov.uk>
2024-03-25 13:34:18 +0000

    radio working partially

--- src/main/controllers/features/sole-trader/whatIsTheBusinessNameController.ts
+++ src/main/controllers/features/sole-trader/whatIsTheBusinessNameController.ts
@@ -4,15 +4,23 @@ import * as config from "../../../config";
 import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { BASE_URL, SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME, SOLE_TRADER_SECTOR_YOU_WORK_IN, SOLE_TRADER_WHERE_DO_YOU_LIVE } from "../../../types/pageURL";
+import { Session } from "@companieshouse/node-session-handler";
+import { USER_DATA } from "../../../common/__utils/constants";
+import { ACSPData } from "../../../model/ACSPData";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
+    const session: Session = req.session as any as Session;
+    const ACSPData : ACSPData = session?.getExtraData(USER_DATA)!;
+
     res.render(config.SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME, {
         previousPage: addLangToUrl(BASE_URL + SOLE_TRADER_WHERE_DO_YOU_LIVE, lang),
         title: "What is the business name?",
         ...getLocaleInfo(locales, lang),
-        currentUrl: BASE_URL + SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME
+        currentUrl: BASE_URL + SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME,
+        firstName: ACSPData?.firstName,
+        lastName: ACSPData?.lastName
     });
 };
 
@@ -20,6 +28,8 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const lang = selectLang(req.query.lang);
         const locales = getLocalesService();
+        const session: Session = req.session as any as Session;
+        const ACSPData : ACSPData = session?.getExtraData(USER_DATA)!;
         const errorList = validationResult(req);
         if (!errorList.isEmpty()) {
             const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
@@ -29,7 +39,9 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
                 payload: req.body,
                 ...getLocaleInfo(locales, lang),
                 currentUrl: BASE_URL + SOLE_TRADER_WHAT_IS_THE_BUSINESS_NAME,
-                ...pageProperties
+                ...pageProperties,
+                firstName: ACSPData?.firstName,
+                lastName: ACSPData?.lastName
             });
         } else {
             const nextPageUrl = addLangToUrl(BASE_URL + SOLE_TRADER_SECTOR_YOU_WORK_IN, lang);
