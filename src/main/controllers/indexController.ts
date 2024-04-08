cb44d386bb77a00008779546a8438b62502fddc9
amanueltedros <atedros@companieshouse.gov.uk>
2024-04-08 11:56:20 +0100

    work in progress

--- src/main/controllers/indexController.ts
+++ src/main/controllers/indexController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import * as config from "../config";
-import { BASE_URL, TYPE_OF_BUSINESS } from "../types/pageURL";
+import { BASE_URL, SOLE_TRADER_WHAT_IS_YOUR_NAME, TYPE_OF_BUSINESS } from "../types/pageURL";
 import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
@@ -16,7 +16,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
 export const post = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const lang = selectLang(req.query.lang);
-        const nextPageUrl = addLangToUrl(BASE_URL + TYPE_OF_BUSINESS, lang);
+        const nextPageUrl = addLangToUrl(BASE_URL + SOLE_TRADER_WHAT_IS_YOUR_NAME, lang);
         res.redirect(nextPageUrl);
     } catch (error) {
         next(error);
