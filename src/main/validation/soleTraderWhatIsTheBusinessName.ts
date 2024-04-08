864bb5f704c9b2faf54ed00e52ae92414624bf02
amanueltedros <atedros@companieshouse.gov.uk>
2024-04-08 17:34:17 +0100

    new validation folder moed to new file

--- src/main/validation/soleTraderWhatIsTheBusinessName.ts
+++ src/main/validation/soleTraderWhatIsTheBusinessName.ts
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 
 const businessNameFormat:RegExp = /^[A-Za-z0-9\-&'.\s]*$/;
 
-export const whatIsTheBusinessNameValidator = [
+export const soleTraderWhatIsTheBusinessNameValidator = [
 
     body("whatIsTheBusinessNameInput").trim().custom((value, { req }) => {
         if (req.body.whatsTheBusinessNameRadio === "A Different Name" && value === "") {
