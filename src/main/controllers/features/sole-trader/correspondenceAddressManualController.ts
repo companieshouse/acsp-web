b789a18a3cd05bea9ce1af677bd51270a40f42d0
sampankumar <skumar@companieshouse.gov.uk>
2024-04-11 10:28:18 +0100

    Removing code smells

--- src/main/controllers/features/sole-trader/correspondenceAddressManualController.ts
+++ src/main/controllers/features/sole-trader/correspondenceAddressManualController.ts
@@ -4,10 +4,9 @@ import * as config from "../../../config";
 import { FormattedValidationErrors, formatValidationError } from "../../../validation/validation";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { SOLE_TRADER_CORRESPONDENCE_ADDRESS_CONFIRM, SOLE_TRADER_AUTO_LOOKUP_ADDRESS, SOLE_TRADER_MANUAL_CORRESPONDENCE_ADDRESS, BASE_URL } from "../../../types/pageURL";
-import { Address } from "../../../model/Address";
 import { ACSPData } from "../../../model/ACSPData";
 import { Session } from "@companieshouse/node-session-handler";
-import { USER_DATA, ADDRESS_LIST } from "../../../common/__utils/constants";
+import { USER_DATA } from "../../../common/__utils/constants";
 import { CorrespondenceAddressManualService } from "../../../../main/services/correspondence-address/correspondence-address-manual";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
