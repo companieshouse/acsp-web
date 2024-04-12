97a22146c6151b6604c73f12b3f4293d827ee23d
sardhani <sardhani@companieshouse.gov.uk>
2024-04-12 11:41:40 +0100

    fix unit tests

--- src/test/src/controllers/soleTraderSelectAmlSupervisorController.test.ts
+++ src/test/src/controllers/soleTraderSelectAmlSupervisorController.test.ts
@@ -1,29 +1,37 @@
 import mocks from "../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../main/app";
-import { SOLE_TRADER_SELECT_AML_SUPERVISOR, BASE_URL } from "../../../main/types/pageURL";
+import { SOLE_TRADER_SELECT_AML_SUPERVISOR, AML_MEMBERSHIP_NUMBER, BASE_URL } from "../../../main/types/pageURL";
 
 jest.mock("@companieshouse/api-sdk-node");
 const router = supertest(app);
 
 describe("GET" + SOLE_TRADER_SELECT_AML_SUPERVISOR, () => {
     it("should return status 200", async () => {
-        await router.get(BASE_URL + SOLE_TRADER_SELECT_AML_SUPERVISOR).expect(200);
+        const res =await router.get(BASE_URL + SOLE_TRADER_SELECT_AML_SUPERVISOR);
+        expect(res.status).toBe(200);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+        expect(res.text).toContain("Which Anti-Money Laundering (AML) supervisory bodies are you registered with?");
     });
 });
 
 // Test for correct form details entered, will return 302 after redirecting to the next page.
 describe("POST" + SOLE_TRADER_SELECT_AML_SUPERVISOR, () => {
     it("should return status 302 after redirect", async () => {
-        await router.post(BASE_URL + SOLE_TRADER_SELECT_AML_SUPERVISOR).send({ "AML-supervisory-bodies": "ACCA" }).expect(302);
+         const res = await router.post(BASE_URL + SOLE_TRADER_SELECT_AML_SUPERVISOR).send({ "AML-supervisory-bodies": "ACCA" });
+         expect(res.status).toBe(302);
+         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+         expect(res.header.location).toBe(BASE_URL + AML_MEMBERSHIP_NUMBER + "?lang=en");
     });
 });
 
 // Test for incorrect form details entered, will return 400.
 describe("POST" + SOLE_TRADER_SELECT_AML_SUPERVISOR, () => {
     it("should return status 400 after incorrect data entered", async () => {
-        await router.post(BASE_URL + SOLE_TRADER_SELECT_AML_SUPERVISOR).send({ "AML-supervisory-bodies": "" }).expect(400);
+         const res = await router.post(BASE_URL + SOLE_TRADER_SELECT_AML_SUPERVISOR).send({ "AML-supervisory-bodies": "" });
+         expect(res.status).toBe(400);
+         expect(res.text).toContain("Select all AML supervisory bodies you are registered with");
     });
 });
