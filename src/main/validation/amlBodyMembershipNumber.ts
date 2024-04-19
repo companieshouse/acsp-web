653a68cb1f86c9ee3b6117cb6bf99ea5a5d60632
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-04-19 09:23:03 +0100

    -build screen for AML body details

--- src/main/validation/amlBodyMembershipNumber.ts
+++ src/main/validation/amlBodyMembershipNumber.ts
@@ -1,3 +1,12 @@
+
+/*import { body } from "express-validator";
+
+export const amlBodyMembershipNumberControllerValidator = [
+    body("membershipNumber_1").notEmpty().withMessage("amlIDNumberInput"),
+    body("membershipNumber_2").notEmpty().withMessage("amlIDNumberInput"),
+    body("membershipNumber_3").notEmpty().withMessage("amlIDNumberInput")
+];*/
+
 import { Session } from "@companieshouse/node-session-handler";
 import { body, ValidationChain } from "express-validator";
 import { AML_SUPERVISOR_SELECTED } from "../common/__utils/constants";
@@ -10,17 +19,12 @@ const customValidator = (value: any, { req }: any) => {
         for (let i = 0; i < selectedAMLSupervisoryBodies.length; i++) {
             const bodyName = selectedAMLSupervisoryBodies[i];
             const validationRule = body(`membershipNumber_${i + 1}`).trim().notEmpty().withMessage("amlIDNumberInput").bail();
-            amlBodyMembershipNumberControllerValidator.push(validationRule);
+
         }
     }
-};
+
+
+}
+
 
 amlBodyMembershipNumberControllerValidator.push(body('*').custom(customValidator));
-
-
-/*import { body } from "express-validator";
-
-export const amlBodyMembershipNumberControllerValidator = [
-    body("membershipNumber_1").notEmpty().withMessage("amlIDNumberInput")
-];*/
-
