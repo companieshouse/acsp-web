c9025b31ddbf2b906e98cc7b70f5548f98d58e95
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-04-26 16:00:53 +0100

    -build screen for AML body details

---
+++ src/main/services/amlSupervisoryBody/amlBodyService.ts
@@ -0,0 +1,13 @@
+import { Request } from "express";
+import { Session } from "@companieshouse/node-session-handler";
+import { AML_SUPERVISOR_SELECTED } from "../../common/__utils/constants";
+
+export const saveSelectedAML = (session: Session, req: Request) => {
+    const selectedAMLSupervisoryBodies = req.body["AML-supervisory-bodies"];
+    if (selectedAMLSupervisoryBodies instanceof Array) {
+        session.setExtraData(AML_SUPERVISOR_SELECTED, selectedAMLSupervisoryBodies);
+    } else {
+        const selectedAML = [selectedAMLSupervisoryBodies];
+        session.setExtraData(AML_SUPERVISOR_SELECTED, selectedAML);
+    }
+};
