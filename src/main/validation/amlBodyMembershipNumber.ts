4c632e937e10af495da0a22fec80281b7334ca04
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-04-29 17:28:09 +0100

    Add unit test

--- src/main/validation/amlBodyMembershipNumber.ts
+++ src/main/validation/amlBodyMembershipNumber.ts
@@ -1,8 +1,10 @@
 import { body, ValidationChain } from "express-validator";
 
+const amlSupervisoryBodiesOptions = 26;
+
 const amlBodyMembershipNumberControllerValidator = () : ValidationChain[] => {
     const amlBodyMembershipNumberErrors : ValidationChain[] = [];
-    for (let i = 1; i < 26; i++) {
+    for (let i = 1; i < amlSupervisoryBodiesOptions ; i++) {
         amlBodyMembershipNumberErrors.push(
             (body(`membershipNumber_${i}`).if(body(`membershipNumber_${i}`).exists()).trim().notEmpty().withMessage("amlIDNumberInput")));
     }
