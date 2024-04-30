31616fdafb65b6485a67e6a73c4163744a192e82
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-04-30 09:53:05 +0100

    -build screen for AML body details

--- src/main/validation/amlBodyMembershipNumber.ts
+++ src/main/validation/amlBodyMembershipNumber.ts
@@ -3,8 +3,8 @@ import { AMLSupervisoryBodies } from "../../main/model/AMLSupervisoryBodies";
 
 const amlBodyMembershipNumberControllerValidator = (): ValidationChain[] => {
     const amlBodyMembershipNumberErrors: ValidationChain[] = [];
-    const numberOfamlSupervisoryBodiesOptions = Object.keys(AMLSupervisoryBodies).length/ 2;
-    for (let i = 1; i <= numberOfamlSupervisoryBodiesOptions; i++) {
+    const numberOfamlSupervisoryBodies = Object.keys(AMLSupervisoryBodies).length / 2;
+    for (let i = 1; i <= numberOfamlSupervisoryBodies; i++) {
         amlBodyMembershipNumberErrors.push(
             (body(`membershipNumber_${i}`).if(body(`membershipNumber_${i}`).exists()).trim().notEmpty().withMessage("amlIDNumberInput")));
     }
