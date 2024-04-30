e041a1d9841a6dc14d40ac0ad3dcc8a8759b6142
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-04-30 11:13:33 +0100

    Req change

--- src/main/controllers/features/limited/companyInactiveController.ts
+++ src/main/controllers/features/limited/companyInactiveController.ts
@@ -3,13 +3,13 @@ import * as config from "../../../config";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { LIMITED_WHAT_IS_THE_COMPANY_NUMBER, LIMITED_COMPANY_INACTIVE, BASE_URL } from "../../../types/pageURL";
 import { Session } from "@companieshouse/node-session-handler";
-import { COMPANY } from "../../../common/__utils/constants";
+import { COMPANY_DETAILS } from "../../../common/__utils/constants";
 import { Company } from "../../../model/Company";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
 
     const session: Session = req.session as any as Session;
-    const company : Company = session?.getExtraData(COMPANY)!;
+    const company : Company = session?.getExtraData(COMPANY_DETAILS)!;
 
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
