0f6ed98b6b99b14fd4de0940c3455c3ac4c5ae3a
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-05-01 16:06:47 +0100

    Add unit test

--- src/test/src/controllers/limitedAddressCorrespondanceSelectorController.test.ts
+++ src/test/src/controllers/limitedAddressCorrespondanceSelectorController.test.ts
@@ -1,14 +1,14 @@
 import mocks from "../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../main/app";
-import { UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS, BASE_URL, UNINCORPORATED_SELECT_AML_SUPERVISOR, UNINCORPORATED_CORRESPONDENCE_ADDRESS_LOOKUP } from "../../../main/types/pageURL";
+import { BASE_URL, LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS, LIMITED_CORRESPONDENCE_ADDRESS_LOOKUP, LIMITED_SECTOR_YOU_WORK_IN } from "../../../main/types/pageURL";
 
 jest.mock("@companieshouse/api-sdk-node");
 const router = supertest(app);
 
-describe("GET " + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS, () => {
+describe("GET " + LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS , () => {
     it("should render the correspondence address selector page with status 200", async () => {
-        const res = await router.get(BASE_URL + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS);
+        const res = await router.get(BASE_URL + LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS);
         expect(res.status).toBe(200);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
@@ -16,10 +16,10 @@ describe("GET " + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS, () => {
     });
 });
 
-describe("POST " + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS, () => {
+describe("POST " + LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS , () => {
     it("should render the correspondence address selector page with validation errors", async () => {
         const res = await router
-            .post(BASE_URL + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS)
+            .post(BASE_URL + LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS)
             .send({ addressSelectorRadio: "" });
         expect(res.status).toBe(400);
         expect(res.text).toContain("What is the correspondence address?");
@@ -28,17 +28,17 @@ describe("POST " + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS, () => {
 
     it("should redirect to correspondence-address-lookup page when address option is CORRESPONDANCE_ADDRESS", async () => {
         const res = await router
-            .post(BASE_URL + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS)
+            .post(BASE_URL + LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS )
             .send({ addressSelectorRadio: "CORRESPONDANCE_ADDRESS" });
         expect(res.status).toBe(302);
-        expect(res.header.location).toBe(BASE_URL + UNINCORPORATED_SELECT_AML_SUPERVISOR + "?lang=en");
+        expect(res.header.location).toBe(BASE_URL + LIMITED_SECTOR_YOU_WORK_IN + "?lang=en");
     });
 
     it("should redirect to correspondence-address-lookup page when address option is DIFFERENT_ADDRESS", async () => {
         const res = await router
-            .post(BASE_URL + UNINCORPORATED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS)
+            .post(BASE_URL + LIMITED_WHAT_IS_THE_CORRESPONDENCE_ADDRESS )
             .send({ addressSelectorRadio: "DIFFERENT_ADDRESS" });
         expect(res.status).toBe(302);
-        expect(res.header.location).toBe(BASE_URL + UNINCORPORATED_CORRESPONDENCE_ADDRESS_LOOKUP + "?lang=en");
+        expect(res.header.location).toBe(BASE_URL + LIMITED_CORRESPONDENCE_ADDRESS_LOOKUP + "?lang=en");
     });
 });
