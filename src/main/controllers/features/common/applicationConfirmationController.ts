aaa952bab9853e774ad2d662408591e8b2412a12
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-05-03 11:17:19 +0100

    get acsp data for applicationConfirmationController

--- src/main/controllers/features/common/applicationConfirmationController.ts
+++ src/main/controllers/features/common/applicationConfirmationController.ts
@@ -4,14 +4,14 @@ import * as config from "../../../config";
 import { CONFIRMATION, BASE_URL } from "../../../types/pageURL";
 import { Session } from "@companieshouse/node-session-handler";
 import { ACSPData } from "../../../model/ACSPData";
-import { USER_DATA } from "../../../common/__utils/constants";
+import { SUBMISSION_ID, USER_DATA } from "../../../common/__utils/constants";
+import { getAcspRegistration } from "main/services/acspRegistrationService";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const session: Session = req.session as any as Session;
-    const acspData: ACSPData = session.getExtraData(USER_DATA)!;
-
+    const acspData = await getAcspRegistration(session, session.getExtraData(SUBMISSION_ID)!, res.locals.userEmail);
     res.render(config.APPLICATION_CONFIRMATION, {
         title: "Application submitted",
         ...getLocaleInfo(locales, lang),
