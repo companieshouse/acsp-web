45791659267c79cfa30aa3bee8468653557db559
sampankumar <skumar@companieshouse.gov.uk>
2024-05-20 17:12:06 +0100

    Fixing failing test

--- src/test/src/controllers/unincorporatedCorrespondenceAddressList.test.ts
+++ src/test/src/controllers/unincorporatedCorrespondenceAddressList.test.ts
@@ -21,6 +21,8 @@ describe("POST" + UNINCORPORATED_CORRESPONDENCE_ADDRESS_LIST, () => {
     it("should return status 400 after incorrect data entered", async () => {
         const res = await router.post(BASE_URL + UNINCORPORATED_CORRESPONDENCE_ADDRESS_LIST).send({ correspondenceAddress: "" });
         expect(res.status).toBe(400);
+        expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
         expect(res.text).toContain("Select the correspondence address");
     });
 });
