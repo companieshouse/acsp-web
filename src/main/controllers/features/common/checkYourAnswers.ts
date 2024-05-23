192542b547704a71bd7e6060133df4ab3afd10ed
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-05-23 10:03:12 +0100

    Updated check your answers screen to get AML bodies from mongodb

--- src/main/controllers/features/common/checkYourAnswers.ts
+++ src/main/controllers/features/common/checkYourAnswers.ts
@@ -4,35 +4,41 @@ import * as config from "../../../config";
 import { AML_MEMBERSHIP_NUMBER, BASE_URL, CHECK_YOUR_ANSWERS, AML_BODY_DETAILS_CONFIRM, CONFIRMATION } from "../../../types/pageURL";
 import { Session } from "@companieshouse/node-session-handler";
 import { ACSPData } from "../../../model/ACSPData";
-import { ANSWER_DATA, NO_PAYMENT_RESOURCE_ERROR, SUBMISSION_ID, USER_DATA } from "../../../common/__utils/constants";
+import { ANSWER_DATA, GET_ACSP_REGISTRATION_DETAILS_ERROR, NO_PAYMENT_RESOURCE_ERROR, SUBMISSION_ID, USER_DATA } from "../../../common/__utils/constants";
 import { Answers } from "../../../model/Answers";
 import { closeTransaction } from "../../../services/transactions/transaction_service";
 import { ApiResponse } from "@companieshouse/api-sdk-node/dist/services/resource";
 import { Payment } from "@companieshouse/api-sdk-node/dist/services/payment";
 import { startPaymentsSession } from "../../../services/paymentService";
 import { createAndLogError } from "../../../utils/logger";
+import logger from "../../../../../lib/Logger";
+import { ErrorService } from "../../../services/errorService";
+import { getAcspRegistration } from "../../../services/acspRegistrationService";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const session: Session = req.session as any as Session;
-    const acspData: ACSPData = session.getExtraData(USER_DATA)!;
-    const detailsAnswers: Answers = session.getExtraData(ANSWER_DATA)!;
-    const amlDetails = new Map<string, string>();
-    amlDetails.set("Association of Chartered Certified Accountants (ACCA)", "12345678");
-    amlDetails.set("Chartered Institute of Legal Executives (CILEx)", "08297534784635");
-    amlDetails.set("HMRC", "859324768974385634858");
-    res.render(config.CHECK_YOUR_ANSWERS, {
-        title: "Check your answers before sending your application",
-        ...getLocaleInfo(locales, lang),
-        currentUrl: BASE_URL + CHECK_YOUR_ANSWERS,
-        previousPage: addLangToUrl(BASE_URL + AML_BODY_DETAILS_CONFIRM, lang),
-        editAML: addLangToUrl(BASE_URL + AML_MEMBERSHIP_NUMBER, lang),
-        typeOfBusiness: acspData.typeOfBusiness,
-        detailsAnswers,
-        lang,
-        amlDetails
-    });
+    const currentUrl = BASE_URL + CHECK_YOUR_ANSWERS;
+    try {
+        const detailsAnswers: Answers = session.getExtraData(ANSWER_DATA)!;
+        const acspData = await getAcspRegistration(session, session.getExtraData(SUBMISSION_ID)!, res.locals.userId);
+        res.render(config.CHECK_YOUR_ANSWERS, {
+            title: "Check your answers before sending your application",
+            ...getLocaleInfo(locales, lang),
+            currentUrl,
+            previousPage: addLangToUrl(BASE_URL + AML_BODY_DETAILS_CONFIRM, lang),
+            editAML: addLangToUrl(BASE_URL + AML_MEMBERSHIP_NUMBER, lang),
+            typeOfBusiness: acspData.typeOfBusiness,
+            detailsAnswers,
+            lang,
+            amlDetails: acspData?.amlSupervisoryBodies
+        });
+    } catch {
+        logger.error(GET_ACSP_REGISTRATION_DETAILS_ERROR);
+        const error = new ErrorService();
+        error.renderErrorPage(res, locales, lang, currentUrl);
+    }
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
