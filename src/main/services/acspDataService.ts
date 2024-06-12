7163c5aac0cc79fd194c07293841d095a15377b8
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-06-12 11:23:35 +0100

    Removed code smell

--- src/main/services/acspDataService.ts
+++ src/main/services/acspDataService.ts
@@ -1,6 +1,6 @@
 import { AcspData } from "@companieshouse/api-sdk-node/dist/services/acsp";
 import { Session } from "@companieshouse/node-session-handler";
-import { SUBMISSION_ID, USER_DATA } from "../common/__utils/constants";
+import { SUBMISSION_ID } from "../common/__utils/constants";
 import { postAcspRegistration, putAcspRegistration } from "./acspRegistrationService";
 import logger from "../../../lib/Logger";
 
