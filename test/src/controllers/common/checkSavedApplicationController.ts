a4ec970f64556b4fe1395e20a6178a3ce87e9333
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-06-17 14:34:28 +0100

    Added error page to checkSavedApplicationController

--- test/src/controllers/common/checkSavedApplicationController.ts
+++ test/src/controllers/common/checkSavedApplicationController.ts
@@ -18,26 +18,36 @@ const hasNoSavedApplication: HttpResponse = {
     status: 404
 };
 
+const errorSavedApplication: HttpResponse = {
+    status: 500
+};
+
 describe("GET " + CHECK_SAVED_APPLICATION, () => {
-    mockGetSavedApplication.mockResolvedValueOnce(hasSavedApplication);
 
     it("should redirect to saved application if exists", async () => {
+        mockGetSavedApplication.mockResolvedValueOnce(hasSavedApplication);
         const response = await router.get(BASE_URL + CHECK_SAVED_APPLICATION);
         expect(response.status).toBe(302);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
         expect(response.header.location).toBe(BASE_URL + SAVED_APPLICATION + "?lang=en");
     });
-});
-
-describe("GET " + CHECK_SAVED_APPLICATION, () => {
-    mockGetSavedApplication.mockResolvedValueOnce(hasNoSavedApplication);
 
     it("should redirect to type of business id saved application does not exists", async () => {
+        mockGetSavedApplication.mockResolvedValueOnce(hasNoSavedApplication);
         const response = await router.get(BASE_URL + CHECK_SAVED_APPLICATION);
         expect(response.status).toBe(302);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
         expect(response.header.location).toBe(BASE_URL + TYPE_OF_BUSINESS + "?lang=en");
+    });
+
+    it("should show the error screen if getSavedApplication fails", async () => {
+        mockGetSavedApplication.mockResolvedValueOnce(errorSavedApplication);
+        const response = await router.get(BASE_URL + CHECK_SAVED_APPLICATION);
+        expect(response.status).toBe(500);
+        expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+        expect(response.text).toContain("Sorry we are experiencing technical difficulties");
     });
 });
