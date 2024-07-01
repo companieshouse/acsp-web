840e9c55784fa033b3905c453daa546602e72e63
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-07-01 14:22:32 +0100

    -fix code smell

--- src/validation/whereDoYouLive.ts
+++ src/validation/whereDoYouLive.ts
@@ -2,13 +2,19 @@ import { body } from "express-validator";
 import countryList from "../../lib/countryList";
 
 export const whereDoYouLiveValidator = [
-    body("countryInput", "whereDoYouLiveNoData").trim().custom((value, { req }) => {
-        if (req.body.whereDoYouLiveRadio === "countryOutsideUK" && value === "") {
-            throw new Error("whereDoYouLiveNoData");
-        } else if (req.body.whereDoYouLiveRadio === "countryOutsideUK" && !countryList.split(";").includes(value.trim())) {
-            throw new Error("whereDoYouLiveEmptyInput");
-
-        } return true;
-    }),
-    body("whereDoYouLiveRadio", "whereDoYouLiveEmptyInput").notEmpty()
+    body("countryInput", "whereDoYouLiveNoData")
+        .trim()
+        .custom((value, { req }) => {
+            if (req.body.whereDoYouLiveRadio === "countryOutsideUK") {
+                if (value === "") {
+                    throw new Error("whereDoYouLiveNoData");
+                } else if (!countryList.split(";").includes(value.trim())) {
+                    throw new Error("whereDoYouLiveEmptyInput");
+                }
+            }
+            return true;
+        }),
+    body("whereDoYouLiveRadio", "whereDoYouLiveEmptyInput")
+        .notEmpty()
 ];
+ 
