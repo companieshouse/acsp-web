3720079f2da3f3ab8775a1e5604454bd00e92aea
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-07-12 14:18:04 +0100

    Added routing for second application kickout screen

--- src/controllers/features/common/checkSavedApplicationController.ts
+++ src/controllers/features/common/checkSavedApplicationController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { selectLang, addLangToUrl, getLocalesService } from "../../../utils/localise";
-import { BASE_URL, CHECK_SAVED_APPLICATION, SAVED_APPLICATION, TYPE_OF_BUSINESS } from "../../../types/pageURL";
+import { BASE_URL, CANNOT_SUBMIT_ANOTHER_APPLICATION, CHECK_SAVED_APPLICATION, SAVED_APPLICATION, TYPE_OF_BUSINESS } from "../../../types/pageURL";
 import { Session } from "@companieshouse/node-session-handler";
 import { getSavedApplication } from "../../../services/acspRegistrationService";
 import logger from "../../../utils/logger";
@@ -13,10 +13,15 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     if (savedApplication.status === 404) {
+        // Direct to start a new journey if no application found or application was rejected
         saveDataInSession(req, "resume_application", false);
         res.redirect(addLangToUrl(BASE_URL + TYPE_OF_BUSINESS, lang));
     } else if (savedApplication.status === 204) {
+        // Application found and can be resumed
         res.redirect(addLangToUrl(BASE_URL + SAVED_APPLICATION, lang));
+    } else if (savedApplication.status === 403) {
+        // Application found and cannot be resumed
+        res.redirect(addLangToUrl(BASE_URL + CANNOT_SUBMIT_ANOTHER_APPLICATION, lang));
     } else {
         logger.error(`internal server error: ` + savedApplication.status);
         const error = new ErrorService();
--- test/src/controllers/common/checkSavedApplicationController.test.ts
+++ test/src/controllers/common/checkSavedApplicationController.test.ts
@@ -1,7 +1,7 @@
 import mocks from "../../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../../src/app";
-import { TYPE_OF_BUSINESS, BASE_URL, CHECK_SAVED_APPLICATION, SAVED_APPLICATION } from "../../../../src/types/pageURL";
+import { TYPE_OF_BUSINESS, BASE_URL, CHECK_SAVED_APPLICATION, SAVED_APPLICATION, CANNOT_SUBMIT_ANOTHER_APPLICATION } from "../../../../src/types/pageURL";
 import { getSavedApplication } from "../../../../src/services/acspRegistrationService";
 import { HttpResponse } from "@companieshouse/api-sdk-node/dist/http";
 
@@ -17,13 +17,17 @@ const hasNoSavedApplication: HttpResponse = {
     status: 404
 };
 
+const applicationProcessing: HttpResponse = {
+    status: 403
+};
+
 const errorSavedApplication: HttpResponse = {
     status: 500
 };
 
 describe("GET " + CHECK_SAVED_APPLICATION, () => {
 
-    it("should redirect to saved application if exists", async () => {
+    it("should redirect to continue with saved application screen if exists", async () => {
         mockGetSavedApplication.mockResolvedValueOnce(hasSavedApplication);
         const response = await router.get(BASE_URL + CHECK_SAVED_APPLICATION);
         expect(response.status).toBe(302);
@@ -32,13 +36,22 @@ describe("GET " + CHECK_SAVED_APPLICATION, () => {
         expect(response.header.location).toBe(BASE_URL + SAVED_APPLICATION + "?lang=en");
     });
 
-    it("should redirect to type of business id saved application does not exists", async () => {
+    it("should redirect to type of business if saved application does not exists", async () => {
         mockGetSavedApplication.mockResolvedValueOnce(hasNoSavedApplication);
         const response = await router.get(BASE_URL + CHECK_SAVED_APPLICATION);
         expect(response.status).toBe(302);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
         expect(response.header.location).toBe(BASE_URL + TYPE_OF_BUSINESS + "?lang=en");
+    });
+
+    it("should redirect to cannot submit another application screen if saved application is processing", async () => {
+        mockGetSavedApplication.mockResolvedValueOnce(applicationProcessing);
+        const response = await router.get(BASE_URL + CHECK_SAVED_APPLICATION);
+        expect(response.status).toBe(302);
+        expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+        expect(response.header.location).toBe(BASE_URL + CANNOT_SUBMIT_ANOTHER_APPLICATION + "?lang=en");
     });
 
     it("should show the error screen if getSavedApplication fails", async () => {
