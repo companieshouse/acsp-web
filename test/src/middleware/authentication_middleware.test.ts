b1447333e2665eccbed628f202f3dba8b4935750
Steve Hicks <shicks@companieshouse.gov.uk>
2024-08-16 15:56:52 +0100

    update auth unit test to use acspProfileCreateAuthMiddleware in authentication

--- test/src/middleware/authentication_middleware.test.ts
+++ test/src/middleware/authentication_middleware.test.ts
@@ -2,13 +2,13 @@
 
 jest.mock("@companieshouse/web-security-node");
 
-import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
+import { acspProfileCreateAuthMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { Request, Response } from "express";
 import { authenticationMiddleware } from "../../../src/middleware/authentication_middleware";
 import { BASE_URL, CHECK_SAVED_APPLICATION } from "../../../src/types/pageURL";
 
 // get handle on mocked function and create mock function to be returned from calling authMiddleware
-const mockAuthMiddleware = authMiddleware as jest.Mock;
+const mockAuthMiddleware = acspProfileCreateAuthMiddleware as jest.Mock;
 const mockAuthReturnedFunction = jest.fn();
 
 // when the mocked authMiddleware is called, make it return a mocked function so we can verify it gets called
@@ -25,8 +25,8 @@ const expectedAuthMiddlewareConfig: AuthOptions = {
 
 describe("authentication middleware tests", () => {
     it("should call CH authentication library", () => {
-        // authenticationMiddleware(req, res, next);
-        // expect(mockAuthMiddleware).toHaveBeenCalledWith(expectedAuthMiddlewareConfig);
-        // expect(mockAuthReturnedFunction).toHaveBeenCalledWith(req, res, next);
+        authenticationMiddleware(req, res, next);
+        expect(mockAuthMiddleware).toHaveBeenCalledWith(expectedAuthMiddlewareConfig);
+        expect(mockAuthReturnedFunction).toHaveBeenCalledWith(req, res, next);
     });
 });
