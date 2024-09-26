0b349c53630567e24b4617fbaf887405553d1a63
cbroadley-ch <cbroadley@companieshouse.gov.uk>
2024-09-26 11:40:39 +0100

    lint fix

--- src/services/checkSavedApplicationService.ts
+++ src/services/checkSavedApplicationService.ts
@@ -11,7 +11,7 @@ import { ErrorService } from "./errorService";
 export const getRedirectionUrl = async (transactionlistResource: Resource<TransactionList>, session: Session, res: Response, locales:any, lang:string): Promise<string> => {
     const transactionList = transactionlistResource.resource;
     const transaction = transactionList?.items[transactionList?.items.length - 1];
-    logger.debug("transactionId: " + transaction?.id); ;
+    logger.debug("transactionId: " + transaction?.id);
     var url = "";
     if (transaction?.status !== CLOSED) {
         logger.debug("application is open");
