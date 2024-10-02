48dd2d9896c029a29003decd5f0fcd4b31b75151
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-10-02 15:52:52 +0100

    Cleared session data when user selects a new type of business

--- src/services/acspDataService.ts
+++ src/services/acspDataService.ts
@@ -1,13 +1,11 @@
 import { AcspData, AcspDataDto } from "@companieshouse/api-sdk-node/dist/services/acsp";
 import { Session } from "@companieshouse/node-session-handler";
-import { APPLICATION_ID, SUBMISSION_ID } from "../common/__utils/constants";
+import { APPLICATION_ID, SUBMISSION_ID, USER_DATA } from "../common/__utils/constants";
 import { postAcspRegistration, putAcspRegistration } from "./acspRegistrationService";
 import logger from "../utils/logger";
 
 export class AcspDataService {
     async saveAcspData (session: Session, acspData: AcspData, selectedOption?: string): Promise<void> {
-        // eslint-disable-next-line camelcase
-        const userId = session?.data?.signin_info?.user_profile?.id!;
         try {
             if (acspData === undefined) {
                 acspData = {
@@ -19,7 +17,7 @@ export class AcspDataService {
                 session.setExtraData(APPLICATION_ID, response.id);
             } else {
                 if (selectedOption !== undefined) {
-                    acspData.typeOfBusiness = selectedOption;
+                    acspData = this.typeOfBusinessChange(session, acspData, selectedOption);
                 }
                 // save data to mongodb
                 await putAcspRegistration(session, session.getExtraData(SUBMISSION_ID)!, acspData);
@@ -28,5 +26,29 @@ export class AcspDataService {
             logger.error("Error saving ACSP data " + JSON.stringify(error));
             return Promise.reject(error);
         }
+    }
+
+    typeOfBusinessChange (session:Session, acspData: AcspData, selectedOption: string): AcspData {
+        if (acspData.typeOfBusiness !== selectedOption) {
+            session.deleteExtraData(USER_DATA);
+            const clearedAcspData: AcspData = {
+                ...acspData,
+                typeOfBusiness: selectedOption,
+                registeredOfficeAddress: undefined,
+                roleType: undefined,
+                verified: undefined,
+                businessName: undefined,
+                workSector: undefined,
+                amlSupervisoryBodies: undefined,
+                companyDetails: undefined,
+                companyAuthCodeProvided: undefined,
+                howAreYouRegisteredWithAml: undefined,
+                applicantDetails: undefined
+            };
+            acspData = clearedAcspData;
+        } else {
+            acspData.typeOfBusiness = selectedOption;
+        }
+        return acspData;
     }
 }
