659cce12b7bcefe68a38cd1ee4cebb32f593c6cf
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-10-03 10:17:58 +0100

    Added unit tests for acspDataService

--- test/src/services/acspDataService.test.ts
+++ test/src/services/acspDataService.test.ts
@@ -3,10 +3,16 @@ import { Session } from "@companieshouse/node-session-handler";
 import { AcspDataService } from "../../../src/services/acspDataService";
 import { getSessionRequestWithPermission } from "../../mocks/session.mock";
 import { createRequest, MockRequest } from "node-mocks-http";
-import { USER_DATA } from "../../../src/common/__utils/constants";
+import { APPLICATION_ID, USER_DATA } from "../../../src/common/__utils/constants";
 import { AcspData } from "@companieshouse/api-sdk-node/dist/services/acsp";
+import { postAcspRegistration, putAcspRegistration } from "../../../src/services/acspRegistrationService";
+
+jest.mock("../../../src/services/acspRegistrationService");
 
 const service = new AcspDataService();
+
+const mockPutAcspRegistration = putAcspRegistration as jest.Mock;
+const mockPostAcspRegistration = postAcspRegistration as jest.Mock;
 
 const mockAcspData: AcspData = {
     id: "1234",
@@ -17,7 +23,11 @@ const mockAcspData: AcspData = {
     }
 };
 
-describe("typeOfBusinessChange tests", () => {
+const mockPostResponce = {
+    id: "12345"
+};
+
+describe("AcspDataService tests", () => {
     let req: MockRequest<Request>;
     beforeEach(() => {
         req = createRequest({
@@ -27,31 +37,56 @@ describe("typeOfBusinessChange tests", () => {
         const session = getSessionRequestWithPermission();
         req.session = session;
     });
-    it("should return acspData if typeOfBusiness is the same", () => {
-        const session: Session = req.session as any as Session;
-        session.setExtraData(USER_DATA, mockAcspData);
-        const retunredAcspData = service.typeOfBusinessChange(session, mockAcspData, "SOLE_TRADER");
-        expect(retunredAcspData).toEqual(mockAcspData);
+
+    describe("saveAcspData tests", () => {
+        it("should call POST registration if acspData is undefined", () => {
+            const session: Session = req.session as any as Session;
+            const acspData: AcspData | undefined = undefined;
+            mockPostAcspRegistration.mockResolvedValueOnce(mockPostResponce);
+            service.saveAcspData(session, acspData!, "SOLE_TRADER");
+            expect(mockPostAcspRegistration).toHaveBeenCalled();
+        });
+
+        it("should call PUT registration if acspData is not undefined", () => {
+            const session: Session = req.session as any as Session;
+            service.saveAcspData(session, mockAcspData, "SOLE_TRADER");
+            expect(mockPutAcspRegistration).toHaveBeenCalled();
+        });
+
+        it("should call PUT registration if acspData is not undefined", () => {
+            const session: Session = req.session as any as Session;
+            service.saveAcspData(session, mockAcspData);
+            expect(mockPutAcspRegistration).toHaveBeenCalled();
+        });
     });
 
-    it("should clear USER_DATA and update acspData if typeOfBusiness changes", () => {
-        const session: Session = req.session as any as Session;
-        session.setExtraData(USER_DATA, mockAcspData);
-        const retunredAcspData = service.typeOfBusinessChange(session, mockAcspData, "PARTNERSHIP");
-        expect(retunredAcspData).toEqual({
-            id: "1234",
-            typeOfBusiness: "PARTNERSHIP",
-            amlSupervisoryBodies: undefined,
-            applicantDetails: undefined,
-            businessName: undefined,
-            companyAuthCodeProvided: undefined,
-            companyDetails: undefined,
-            howAreYouRegisteredWithAml: undefined,
-            registeredOfficeAddress: undefined,
-            roleType: undefined,
-            verified: undefined,
-            workSector: undefined
+    describe("typeOfBusinessChange tests", () => {
+        it("should return acspData if typeOfBusiness is the same", () => {
+            const session: Session = req.session as any as Session;
+            session.setExtraData(USER_DATA, mockAcspData);
+            const retunredAcspData = service.typeOfBusinessChange(session, mockAcspData, "SOLE_TRADER");
+            expect(retunredAcspData).toEqual(mockAcspData);
         });
-        expect(session.getExtraData(USER_DATA)).toEqual(undefined);
+
+        it("should clear USER_DATA and update acspData if typeOfBusiness changes", () => {
+            const session: Session = req.session as any as Session;
+            session.setExtraData(USER_DATA, mockAcspData);
+            const retunredAcspData = service.typeOfBusinessChange(session, mockAcspData, "PARTNERSHIP");
+            expect(retunredAcspData).toEqual({
+                id: "1234",
+                typeOfBusiness: "PARTNERSHIP",
+                amlSupervisoryBodies: undefined,
+                applicantDetails: undefined,
+                businessName: undefined,
+                companyAuthCodeProvided: undefined,
+                companyDetails: undefined,
+                howAreYouRegisteredWithAml: undefined,
+                registeredOfficeAddress: undefined,
+                roleType: undefined,
+                verified: undefined,
+                workSector: undefined
+            });
+            expect(session.getExtraData(USER_DATA)).toEqual(undefined);
+        });
     });
 });
