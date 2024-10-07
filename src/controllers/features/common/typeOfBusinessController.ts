01a962f0c2f3c40bae72ef23c4dea97f28fa7b75
rdas <rdas@companieshouse.gov.uk>
2024-10-07 14:53:51 +0100

    Issues on clicking browser back AML stop screen for Limited journey-IDVA5 1277

--- src/controllers/features/common/typeOfBusinessController.ts
+++ src/controllers/features/common/typeOfBusinessController.ts
@@ -26,8 +26,6 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     const previousPage: string = addLangToUrl(BASE_URL, lang);
     const currentUrl: string = BASE_URL + TYPE_OF_BUSINESS;
 
-    const prevurl = await getPreviousPageUrl(req, BASE_URL);
-    const defaultBusinessType: boolean = prevurl?.includes(LIMITED_BUSINESS_MUSTBE_AML_REGISTERED_KICKOUT);
     try {
         // create transaction record
         if (existingTransactionId === undefined || JSON.stringify(existingTransactionId) === "{}") {
@@ -44,6 +42,8 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
             const acspData = await getAcspRegistration(session, session.getExtraData(SUBMISSION_ID)!, res.locals.applicationId);
             if (acspData !== undefined) {
                 saveDataInSession(req, USER_DATA, acspData);
+                const prevurl = await getPreviousPageUrl(req, BASE_URL);
+                const defaultBusinessType: boolean = prevurl?.includes(LIMITED_BUSINESS_MUSTBE_AML_REGISTERED_KICKOUT);
                 if (defaultBusinessType) {
                     typeOfBusiness = "SOLE_TRADER";
                 } else if (acspData.typeOfBusiness === "UNINCORPORATED" || acspData.typeOfBusiness === "CORPORATE_BODY") {
