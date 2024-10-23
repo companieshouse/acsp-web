00a95c1f87fd35209c53d15a898ac6e60d8b833a
rdas <rdas@companieshouse.gov.uk>
2024-10-23 13:15:54 +0100

    IDVA5-1337 [Registration] Sole Trader Only IDV - Feature Flag

--- test/src/middleware/authentication_middleware_sole_trader.test.ts
+++ test/src/middleware/authentication_middleware_sole_trader.test.ts
@@ -2,13 +2,13 @@
 
 jest.mock("@companieshouse/web-security-node");
 
-import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
+import { acspProfileCreateAuthMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { Request, Response } from "express";
-import { authenticationMiddleware } from "../../../src/middleware/authentication_middleware";
+import { authenticationMiddlewareForSoleTrader } from "../../../src/middleware/authentication_middleware_sole_trader";
 import { BASE_URL, CHECK_SAVED_APPLICATION } from "../../../src/types/pageURL";
 
 // get handle on mocked function and create mock function to be returned from calling authMiddleware
-const mockAuthMiddleware = authMiddleware as jest.Mock;
+const mockAuthMiddleware = acspProfileCreateAuthMiddleware as jest.Mock;
 const mockAuthReturnedFunction = jest.fn();
 
 // when the mocked authMiddleware is called, make it return a mocked function so we can verify it gets called
@@ -25,7 +25,7 @@ const expectedAuthMiddlewareConfig: AuthOptions = {
 
 describe("authentication middleware tests", () => {
     it("should call CH authentication library", () => {
-        authenticationMiddleware(req, res, next);
+        authenticationMiddlewareForSoleTrader(req, res, next);
         expect(mockAuthMiddleware).toHaveBeenCalledWith(expectedAuthMiddlewareConfig);
         expect(mockAuthReturnedFunction).toHaveBeenCalledWith(req, res, next);
     });
