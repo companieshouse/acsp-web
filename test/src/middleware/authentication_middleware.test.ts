91d7e52fb3c4d073610b33ec902d3a6035021be9
rdas <rdas@companieshouse.gov.uk>
2024-10-25 13:11:42 +0100

    IDVA5-1337 [Registration] Sole Trader Only IDV - Feature Flag

--- test/src/middleware/authentication_middleware.test.ts
+++ test/src/middleware/authentication_middleware.test.ts
@@ -1,19 +1,16 @@
 /* eslint-disable import/first */
 jest.mock("@companieshouse/web-security-node");
 process.env.FEATURE_FLAG_DISABLE_MANDATORY_VERIFICATION = "false";
-import { acspProfileCreateAuthMiddleware, authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
+import { acspProfileCreateAuthMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { Request, Response } from "express";
 import { authenticationMiddleware } from "../../../src/middleware/authentication_middleware";
 import { BASE_URL, CHECK_SAVED_APPLICATION } from "../../../src/types/pageURL";
-// get handle on mocked function and create mock function to be returned from calling authMiddleware
-const mockAuthMiddleware = authMiddleware as jest.Mock;
-const mockAuthReturnedFunctionAuthMiddleware = jest.fn();
 
+// get handle on mocked function and create mock function to be returned from calling authMiddleware
 const mockAcspProfileCreateAuthMiddleware = acspProfileCreateAuthMiddleware as jest.Mock;
 const mockAuthReturnedFunctionAcspProfileCreateAuthMiddleware = jest.fn();
 
 // when the mocked authMiddleware is called, make it return a mocked function so we can verify it gets called
-mockAuthMiddleware.mockReturnValue(mockAuthReturnedFunctionAuthMiddleware);
 mockAcspProfileCreateAuthMiddleware.mockReturnValue(mockAuthReturnedFunctionAcspProfileCreateAuthMiddleware);
 
 const req: Request = {} as Request;
