c68ce2ecd6e50e442cea567f5204144493431928
sampankumar <skumar@companieshouse.gov.uk>
2024-11-08 15:21:28 +0000

    Fixing the issue with redirection

--- src/middleware/authentication_middleware.ts
+++ src/middleware/authentication_middleware.ts
@@ -3,13 +3,28 @@ import { AuthOptions, acspProfileCreateAuthMiddleware } from "@companieshouse/we
 
 import { CHS_URL } from "../utils/properties";
 import { BASE_URL, CHECK_SAVED_APPLICATION } from "../types/pageURL";
+import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
+import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
+import { ISignInInfo, IUserProfile } from "@companieshouse/node-session-handler/lib/session/model/SessionInterfaces";
 
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
-    const authMiddlewareConfig: AuthOptions = {
-        chsWebUrl: CHS_URL,
-        returnUrl: BASE_URL + CHECK_SAVED_APPLICATION
-    };
+    const signInInfo: ISignInInfo = req.session?.get<ISignInInfo>(SessionKey.SignInInfo) || {};
+    const signedIn: boolean = signInInfo![SignInInfoKeys.SignedIn] === 1;
+
+    let authMiddlewareConfig: AuthOptions;
+
+    if (!signedIn) {
+        authMiddlewareConfig = {
+            chsWebUrl: CHS_URL,
+            returnUrl: BASE_URL + CHECK_SAVED_APPLICATION
+        };
+    } else {
+        authMiddlewareConfig = {
+            chsWebUrl: CHS_URL,
+            returnUrl: req.originalUrl
+        };
+    }
 
     return acspProfileCreateAuthMiddleware(authMiddlewareConfig)(req, res, next);
 };
--- test/src/middleware/authentication_middleware.test.ts
+++ test/src/middleware/authentication_middleware.test.ts
@@ -5,7 +5,10 @@ jest.mock("@companieshouse/web-security-node");
 import { acspProfileCreateAuthMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import { Request, Response } from "express";
 import { authenticationMiddleware } from "../../../src/middleware/authentication_middleware";
-import { BASE_URL, CHECK_SAVED_APPLICATION } from "../../../src/types/pageURL";
+import { BASE_URL, CHECK_SAVED_APPLICATION, LIMITED_WHAT_IS_YOUR_ROLE } from "../../../src/types/pageURL";
+import { getSessionRequestWithPermission } from "../../mocks/session.mock";
+import { USER_DATA, COMPANY_NUMBER } from "../../../src/common/__utils/constants";
+import { Session } from "@companieshouse/node-session-handler";
 
 // get handle on mocked function and create mock function to be returned from calling authMiddleware
 const mockAuthMiddleware = acspProfileCreateAuthMiddleware as jest.Mock;
@@ -23,10 +26,37 @@ const expectedAuthMiddlewareConfig: AuthOptions = {
     returnUrl: BASE_URL + CHECK_SAVED_APPLICATION
 };
 
+const expectedAuthMiddlewareConfigWithWhatisRoleURL: AuthOptions = {
+    chsWebUrl: "http://chs.local",
+    returnUrl: BASE_URL + LIMITED_WHAT_IS_YOUR_ROLE
+};
+
 describe("authentication middleware tests", () => {
     it("should call CH authentication library", () => {
         authenticationMiddleware(req, res, next);
         expect(mockAuthMiddleware).toHaveBeenCalledWith(expectedAuthMiddlewareConfig);
         expect(mockAuthReturnedFunction).toHaveBeenCalledWith(req, res, next);
     });
+
+    it("should call CH authentication library with Limited URL when session is available ", () => {
+        let request = {} as Request;
+        const Url = BASE_URL + LIMITED_WHAT_IS_YOUR_ROLE;
+        request = {
+            session: getSessionRequestWithExtraData(true),
+            originalUrl: Url
+        } as unknown as Request;
+        authenticationMiddleware(request, res, next);
+        expect(mockAuthMiddleware).toHaveBeenCalledWith(expectedAuthMiddlewareConfigWithWhatisRoleURL);
+    });
+
 });
+
+function getSessionRequestWithExtraData (value: Boolean): Session {
+    const session = getSessionRequestWithPermission();
+
+    session.setExtraData(USER_DATA, {
+        companyAuthCodeProvided: value
+    });
+    session.setExtraData(COMPANY_NUMBER, "NI038379");
+    return session;
+}
