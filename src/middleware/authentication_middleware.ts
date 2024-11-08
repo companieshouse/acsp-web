6b7168809beed0e551f55bcf97d156053d61cb55
sampankumar <skumar@companieshouse.gov.uk>
2024-11-08 16:32:42 +0000

    Removing code smells

--- src/middleware/authentication_middleware.ts
+++ src/middleware/authentication_middleware.ts
@@ -5,12 +5,12 @@ import { CHS_URL, FEATURE_FLAG_VERIFY_SOLE_TRADER_ONLY } from "../utils/properti
 import { BASE_URL, CHECK_SAVED_APPLICATION } from "../types/pageURL";
 import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
 import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
-import { ISignInInfo, IUserProfile } from "@companieshouse/node-session-handler/lib/session/model/SessionInterfaces";
+import { ISignInInfo } from "@companieshouse/node-session-handler/lib/session/model/SessionInterfaces";
 
 export const authenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
     const signInInfo: ISignInInfo = req.session?.get<ISignInInfo>(SessionKey.SignInInfo) || {};
-    const signedIn: boolean = signInInfo![SignInInfoKeys.SignedIn] === 1;
+    const signedIn: boolean = signInInfo[SignInInfoKeys.SignedIn] === 1;
 
     let authMiddlewareConfig: AuthOptions;
 
