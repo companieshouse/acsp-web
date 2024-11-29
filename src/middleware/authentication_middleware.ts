f5c7f3277982d5d3f63e4428958e36537cb75444
cbroadley-ch <cbroadley@companieshouse.gov.uk>
2024-11-29 16:46:40 +0000

    Adding update acsp details base url to authentication middleware

--- src/middleware/authentication_middleware.ts
+++ src/middleware/authentication_middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { AuthOptions, authMiddleware, acspProfileCreateAuthMiddleware } from "@companieshouse/web-security-node";
 import { isActiveFeature } from "../utils/feature.flag";
 import { CHS_URL, FEATURE_FLAG_VERIFY_SOLE_TRADER_ONLY } from "../utils/properties";
-import { BASE_URL, CHECK_SAVED_APPLICATION } from "../types/pageURL";
+import { BASE_URL, CHECK_SAVED_APPLICATION, UPDATE_ACSP_DETAILS_BASE_URL } from "../types/pageURL";
 import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
 import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
 import { ISignInInfo } from "@companieshouse/node-session-handler/lib/session/model/SessionInterfaces";
@@ -15,9 +15,15 @@ export const authenticationMiddleware = (req: Request, res: Response, next: Next
     let authMiddlewareConfig: AuthOptions;
 
     if (!signedIn) {
+        let returnUrl = "";
+        if (req.originalUrl === UPDATE_ACSP_DETAILS_BASE_URL) {
+            returnUrl = req.originalUrl;
+        } else {
+            returnUrl = BASE_URL + CHECK_SAVED_APPLICATION;
+        }
         authMiddlewareConfig = {
             chsWebUrl: CHS_URL,
-            returnUrl: BASE_URL + CHECK_SAVED_APPLICATION
+            returnUrl: returnUrl
         };
     } else {
         authMiddlewareConfig = {
