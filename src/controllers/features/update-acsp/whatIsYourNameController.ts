3937356e90f11b801665cb85bf5afb2387f6f03f
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-12-06 14:19:58 +0000

    Added String constant for update service, fixed njk class value, added comment for acspData

--- src/controllers/features/update-acsp/whatIsYourNameController.ts
+++ src/controllers/features/update-acsp/whatIsYourNameController.ts
@@ -11,20 +11,24 @@ import { validationResult } from "express-validator";
 import { formatValidationError, getPageProperties } from "../../../validation/validation";
 import { Session } from "@companieshouse/node-session-handler";
 import { AcspData } from "@companieshouse/api-sdk-node/dist/services/acsp";
-import { USER_DATA } from "../../../common/__utils/constants";
+import { REQ_TYPE_UPDATE_ACSP, USER_DATA } from "../../../common/__utils/constants";
 import { saveDataInSession } from "../../../common/__utils/sessionHelper";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const session: Session = req.session as any as Session;
+    // Ideally, acspData should never be null, but since the updateAcspDetails page is not yet developed,
+    // acspData is always null. To avoid errors and allow testing, we use:
+    // const acspData: AcspData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
+    // This ensures functionality until the page is ready, at which point we can handle null separately.
     const acspData: AcspData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
     const payload = {
         "first-name": acspData.applicantDetails?.firstName,
         "middle-names": acspData.applicantDetails?.middleName,
         "last-name": acspData.applicantDetails?.lastName
     };
-    const reqType = "updateAcsp";
+    const reqType = REQ_TYPE_UPDATE_ACSP;
     res.render(config.WHAT_IS_YOUR_NAME, {
         ...getLocaleInfo(locales, lang),
         currentUrl: UPDATE_ACSP_WHAT_IS_YOUR_NAME,
@@ -38,7 +42,7 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const errorList = validationResult(req);
-    const reqType = "updateAcsp";
+    const reqType = REQ_TYPE_UPDATE_ACSP;
     const previousPage = addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_ACSP_CHANGE_DETAILS, lang);
     if (!errorList.isEmpty()) {
         const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
--- src/controllers/features/update-acsp/whereDoYouLiveController.ts
+++ src/controllers/features/update-acsp/whereDoYouLiveController.ts
@@ -9,17 +9,21 @@ import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../.
 import { saveDataInSession } from "../../../common/__utils/sessionHelper";
 import { AcspData } from "@companieshouse/api-sdk-node/dist/services/acsp";
 import { WhereDoYouLivBodyService } from "../../../services/where-do-you-live/whereDoYouLive";
-import { USER_DATA } from "../../../common/__utils/constants";
+import { REQ_TYPE_UPDATE_ACSP, USER_DATA } from "../../../common/__utils/constants";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const session: Session = req.session as any as Session;
+    // Ideally, acspData should never be null, but since the updateAcspDetails page is not yet developed,
+    // acspData is always null. To avoid errors and allow testing, we use:
+    // const acspData: AcspData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
+    // This ensures functionality until the page is ready, at which point we can handle null separately.
     const acspData: AcspData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
     const currentUrl: string = UPDATE_WHERE_DO_YOU_LIVE;
 
     const { payload, countryInput } = new WhereDoYouLivBodyService().getCountryPayload(acspData);
-    const reqType = "updateAcsp";
+    const reqType = REQ_TYPE_UPDATE_ACSP;
     res.render(config.SOLE_TRADER_WHERE_DO_YOU_LIVE, {
         ...getLocaleInfo(locales, lang),
         previousPage: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_ACSP_CHANGE_DETAILS, lang),
@@ -38,7 +42,7 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const errorList = validationResult(req);
-    const reqType = "updateAcsp";
+    const reqType = REQ_TYPE_UPDATE_ACSP;
     const currentUrl: string = UPDATE_WHERE_DO_YOU_LIVE;
 
     const session: Session = req.session as any as Session;
