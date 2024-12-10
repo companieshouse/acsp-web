928aace6513e02997a2f70bc6b09fcb3461eca0a
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-12-10 10:04:28 +0000

    Added authentication middleware for update acsp service

--- test/mocks/all_middleware_mock.ts
+++ test/mocks/all_middleware_mock.ts
@@ -3,11 +3,13 @@ import mockAuthenticationMiddlewareForSoleTrader from "./authentication_middlewa
 import mockSessionMiddleware from "./session_middleware_mock";
 import mockCompanyAuthenticationMiddleware from "./company_authentication_middleware_mock";
 import mockUKAddressesFromPostcode from "./postcode_lookup_service_mock";
+import mockAcspAuthenticationMiddleware from "./update_acsp_authentication_middleware_mock";
 
 export default {
     mockAuthenticationMiddleware,
     mockAuthenticationMiddlewareForSoleTrader,
     mockSessionMiddleware,
     mockCompanyAuthenticationMiddleware,
-    mockUKAddressesFromPostcode
+    mockUKAddressesFromPostcode,
+    mockAcspAuthenticationMiddleware
 };
