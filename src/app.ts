c3ae0861ce019b731e0c76dc8fb0bbf05446d984
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-12-11 15:34:36 +0000

    Removing feature flag

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,6 @@ import {
     CHS_URL,
     PIWIK_URL,
     PIWIK_SITE_ID,
-    FEATURE_FLAG_ENABLE_UPDATE_ACSP_DETAILS,
     CHS_MONITOR_GUI_URL
 } from "./utils/properties";
 import { BASE_URL, SOLE_TRADER, HEALTHCHECK, ACCESSIBILITY_STATEMENT, UPDATE_ACSP_DETAILS_BASE_URL } from "./types/pageURL";
@@ -26,7 +25,6 @@ import { ErrorService } from "./services/errorService";
 import { updateAcspAuthMiddleware } from "./middleware/update-acsp/update_acsp_authentication_middleware";
 import { updateAcspBaseAuthenticationMiddleware } from "./middleware/update-acsp/update_acsp_base_authentication_middleware";
 import { updateAcspIsOwnerMiddleware } from "./middleware/update-acsp/update_acsp_is_owner_middleware";
-import { isActiveFeature } from "./utils/feature.flag";
 const app = express();
 
 const nunjucksEnv = nunjucks.configure([path.join(__dirname, "views"),
@@ -65,11 +63,10 @@ app.use(`^(?!(${BASE_URL}${HEALTHCHECK}|${BASE_URL}$|${BASE_URL}${ACCESSIBILITY_
 app.use(`^(?!(${BASE_URL}${HEALTHCHECK}|${BASE_URL}$|${BASE_URL}${ACCESSIBILITY_STATEMENT})|(${BASE_URL}${SOLE_TRADER})|(${UPDATE_ACSP_DETAILS_BASE_URL}))*`, authenticationMiddleware);
 app.use(`^(${BASE_URL}${SOLE_TRADER})*`, authenticationMiddlewareForSoleTrader);
 app.use(commonTemplateVariablesMiddleware);
-if (isActiveFeature(FEATURE_FLAG_ENABLE_UPDATE_ACSP_DETAILS)) {
-    app.use(UPDATE_ACSP_DETAILS_BASE_URL, updateAcspBaseAuthenticationMiddleware);
-    app.use(UPDATE_ACSP_DETAILS_BASE_URL, updateAcspAuthMiddleware);
-    app.use(UPDATE_ACSP_DETAILS_BASE_URL, updateAcspIsOwnerMiddleware);
-}
+
+app.use(UPDATE_ACSP_DETAILS_BASE_URL, updateAcspBaseAuthenticationMiddleware);
+app.use(UPDATE_ACSP_DETAILS_BASE_URL, updateAcspAuthMiddleware);
+app.use(UPDATE_ACSP_DETAILS_BASE_URL, updateAcspIsOwnerMiddleware);
 
 // Company Auth redirect
 // const companyAuthRegex = new RegExp(`^${HOME_URL}/.+`);
