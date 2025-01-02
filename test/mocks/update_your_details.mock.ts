d751be514eabe921489901126761acb43f416e5e
rdas <rdas@companieshouse.gov.uk>
2025-01-02 14:32:17 +0000

    IDVA5-1149 - View and update the authorised agent's details

--- test/mocks/update_your_details.mock.ts
+++ test/mocks/update_your_details.mock.ts
@@ -87,3 +87,47 @@ export const mockSoleTraderAcspFullProfile: AcspFullProfile = {
         dateOfBirth: new Date(1990, 10, 15)
     }
 };
+
+export const mockUnincorporatedAcspFullProfile: AcspFullProfile = {
+    number: "AP135790",
+    name: "John Doe",
+    status: "active",
+    type: "unincorporated-entity",
+    notifiedFrom : new Date(1990, 10, 15),
+    deauthorisedFrom: new Date(1990, 10, 15) ,
+    email: "john.doe@example.com",
+    amlDetails: [{
+        supervisoryBody: "financial-conduct-authority-fca",
+        membershipDetails: "Membership ID: FCA654321"
+    }],
+    registeredOfficeAddress: {
+        careOf: "Jane Smith",
+        addressLine1: "456 Another Street",
+        addressLine2: "Floor 2",
+        country: "united-kingdom",
+        locality: "Manchester",
+        poBox: "PO Box 123",
+        postalCode: "M1 2AB",
+        premises: "Another Building",
+        region: "Greater Manchester"
+    },
+    serviceAddress: {
+        careOf: "Jane Smith",
+        addressLine1: "456 Another Street",
+        addressLine2: "Floor 2",
+        country: "united-kingdom",
+        locality: "Manchester",
+        poBox: "PO Box 123",
+        postalCode: "M1 2AB",
+        premises: "Another Building",
+        region: "Greater Manchester"
+    },
+    soleTraderDetails:{
+        forename: "John",
+        otherForenames: "A.",
+        surname: "Doe",
+        nationality: "British",
+        usualResidentialCountry: "united-kingdom",
+        dateOfBirth: new Date(1990, 10, 15)
+    }
+};
--- test/src/services/update_acsp/updateYourDetailsService.test.ts
+++ test/src/services/update_acsp/updateYourDetailsService.test.ts
@@ -5,7 +5,7 @@ import { getProfileDetails } from "../../../../src/services/update-acsp/updateYo
 import { getLocalesService } from "../../../../src/utils/localise";
 import { Session } from "@companieshouse/node-session-handler";
 import { ACSP_DETAILS } from "../../../../src/common/__utils/constants";
-import { mockLimitedAcspFullProfile, mockSoleTraderAcspFullProfile } from "../../../mocks/update_your_details.mock";
+import { mockLimitedAcspFullProfile, mockSoleTraderAcspFullProfile, mockUnincorporatedAcspFullProfile } from "../../../mocks/update_your_details.mock";
 
 describe("CheckYourAnswersService", () => {
     let req: MockRequest<Request>;
@@ -30,7 +30,6 @@ describe("CheckYourAnswersService", () => {
 
         session.setExtraData(ACSP_DETAILS, mockLimitedAcspFullProfile);
         const limitedAnswers = getProfileDetails(req, mockLimitedAcspFullProfile, locales.i18nCh.resolveNamespacesKeys(req.query.lang));
-        console.log(limitedAnswers);
         expect(limitedAnswers).toStrictEqual({
             typeOfBusiness: "limited-company",
             correspondenceEmail: "john.doe@example.com",
@@ -44,7 +43,6 @@ describe("CheckYourAnswersService", () => {
 
         session.setExtraData(ACSP_DETAILS, mockSoleTraderAcspFullProfile);
         const soleTraderAnswers = getProfileDetails(req, mockSoleTraderAcspFullProfile, locales.i18nCh.resolveNamespacesKeys(req.query.lang));
-        console.log(soleTraderAnswers);
         expect(soleTraderAnswers).toStrictEqual({
             typeOfBusiness: "sole-trader",
             correspondenceEmail: "john.doe@example.com",
@@ -52,6 +50,20 @@ describe("CheckYourAnswersService", () => {
             name: "John A. Doe",
             countryOfResidence: "united-kingdom",
             correspondenceAddress: "Another Building 456 Another Street<br>Floor 2<br>Manchester<br>Greater Manchester<br>united-kingdom<br>M1 2AB"
+        });
+    });
+
+    it("should return answers for unincorporated company journey", () => {
+        const session: Session = req.session as any as Session;
+
+        session.setExtraData(ACSP_DETAILS, mockUnincorporatedAcspFullProfile);
+        const unincorporatedAnswers = getProfileDetails(req, mockUnincorporatedAcspFullProfile, locales.i18nCh.resolveNamespacesKeys(req.query.lang));
+        expect(unincorporatedAnswers).toStrictEqual({
+            typeOfBusiness: 'unincorporated-entity',
+            correspondenceEmail: 'john.doe@example.com',
+            businessName: 'John Doe',
+            businessAddress: 'Another Building 456 Another Street<br>Floor 2<br>Manchester<br>Greater Manchester<br>united-kingdom<br>M1 2AB',
+            correspondenceAddress: 'Another Building 456 Another Street<br>Floor 2<br>Manchester<br>Greater Manchester<br>united-kingdom<br>M1 2AB'
         });
     });
 });
