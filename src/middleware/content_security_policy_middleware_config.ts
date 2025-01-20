dce88af4709a7a3e60521f770f6dac61b629ea60
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-01-20 13:10:13 +0000

    Updating form action

--- src/middleware/content_security_policy_middleware_config.ts
+++ src/middleware/content_security_policy_middleware_config.ts
@@ -5,6 +5,7 @@ export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
     const SELF = `'self'`;
     const NONCE = `'nonce-${nonce}'`;
     const ONE_YEAR_SECONDS = 31536000;
+    const CHS_URL_NO_PROTOCAL = removeProtocal(CHS_URL);
 
     return {
         contentSecurityPolicy: {
@@ -15,7 +16,7 @@ export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
                 imgSrc: [CDN_HOST],
                 styleSrc: [NONCE, CDN_HOST],
                 connectSrc: [SELF, PIWIK_URL],
-                formAction: [SELF, PIWIK_CHS_DOMAIN, CHS_URL],
+                formAction: [SELF, PIWIK_CHS_DOMAIN, CHS_URL_NO_PROTOCAL],
                 scriptSrc: [NONCE, CDN_HOST, PIWIK_URL],
                 objectSrc: [`'none'`]
             }
@@ -28,4 +29,8 @@ export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
             includeSubDomains: true
         }
     };
+};
+
+const removeProtocal = (url: string): string => {
+    return url.replace(/https?:\/\//, "");
 };
