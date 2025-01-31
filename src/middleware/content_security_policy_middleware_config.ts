9322971f2739ab50dcf9c8a07e12c8e4549f3e59
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-01-31 13:58:04 +0000

    adding more urls to csp formAction

--- src/middleware/content_security_policy_middleware_config.ts
+++ src/middleware/content_security_policy_middleware_config.ts
@@ -1,5 +1,6 @@
 import { HelmetOptions } from "helmet";
 import { CDN_HOST, PIWIK_URL, PIWIK_CHS_DOMAIN, CHS_URL, ACCOUNT_URL } from "../utils/properties";
+import { BASE_URL, CHECK_SAVED_APPLICATION, SAVED_APPLICATION } from "../types/pageURL";
 
 export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
     const SELF = `'self'`;
@@ -11,6 +12,8 @@ export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
     const OAUTH_CHOOSE_SIGN_IN = `${ACCOUNT_URL}/oauth2/user/choose-your-signin`;
     const OAUTH_USER_CALL_BACK = `${CHS_URL}/user/callback`;
     const REGISTER_URL = `${CHS_URL}/register-as-companies-house-authorised-agent`;
+    const CHECK_SAVED_APPLICAITON_CSP = CHS_URL + BASE_URL + CHECK_SAVED_APPLICATION;
+    const SAVED_APPLICATION_CSP = CHS_URL + BASE_URL + SAVED_APPLICATION;
 
     return {
         contentSecurityPolicy: {
@@ -22,7 +25,8 @@ export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
                 styleSrc: [NONCE, CDN_HOST],
                 connectSrc: [SELF, PIWIK_URL, CHS_URL],
                 formAction: [SELF, REGISTER_URL, CHS_URL, PIWIK_CHS_DOMAIN, OAUTH_USER_CALL_BACK,
-                    CHS_SIGN_IN, OAUTH_AUTHORIZE, OAUTH_CHOOSE_SIGN_IN],
+                    CHS_SIGN_IN, OAUTH_AUTHORIZE, OAUTH_CHOOSE_SIGN_IN,
+                    CHECK_SAVED_APPLICAITON_CSP, SAVED_APPLICATION_CSP],
                 scriptSrc: [NONCE, CDN_HOST, PIWIK_URL],
                 objectSrc: [`'none'`]
             }
