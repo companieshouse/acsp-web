9cd4fc409c8189fc7e7288b6c9d39357a3813b9a
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-02-06 11:29:23 +0000

    Removing code smell

--- src/controllers/features/update-acsp/cancelAnUpdateControllers.ts
+++ src/controllers/features/update-acsp/cancelAnUpdateControllers.ts
@@ -4,6 +4,6 @@ import { UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_YOUR_ANSWERS } from "../../../type
 import { cancelAnUpdate } from "../../../services/update-acsp/cancelAnUpdateService";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-    await cancelAnUpdate(req);
+    cancelAnUpdate(req);
     res.redirect(addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, selectLang(req.query.lang)));
 };
