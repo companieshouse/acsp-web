3fa2c77af18f474b2acab431220f336179776c1c
rdas <rdas@companieshouse.gov.uk>
2025-02-13 08:54:37 +0000

    IDVA5-1707 Remove an AML body

--- test/src/services/update_acsp/amlSupervisorService.test.ts
+++ test/src/services/update_acsp/amlSupervisorService.test.ts
@@ -19,7 +19,7 @@ describe("removeAMLService tests", () => {
         req.session = session;
         session.setExtraData(ACSP_DETAILS, dummyFullProfile);
     });
-    it("should reset the 'usualResidentialCountry' field when cancel query is 'usualResidentialCountry'", () => {
+    it("to mark the aml supervisor details as empty as a temporary removal", () => {
         const session: Session = req.session as any as Session;
         req.query.amlindex = "123456789";
         session.setExtraData(ACSP_DETAILS_UPDATED,
@@ -35,7 +35,7 @@ describe("removeAMLService tests", () => {
         expect(session.getExtraData(ACSP_DETAILS_UPDATED).amlDetails[0].supervisoryBody).toBe("hm-revenue-customs-hmrc");
         expect(session.getExtraData(ACSP_DETAILS_UPDATED).amlDetails[0].membershipDetails).toBe("123456789");
     });
-    it("should reset the 'name' field when cancel query is 'businessName'", () => {
+    it("to undo the aml supervisor details from empty", () => {
         const session: Session = req.session as any as Session;
         req.query.amlindex = "123456789";
         session.setExtraData(ACSP_DETAILS_UPDATED, dummyFullProfile);
