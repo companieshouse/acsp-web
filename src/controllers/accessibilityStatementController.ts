5db37f0c7ea0a3a410731154f3bf81967289b361
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-03-19 11:24:57 +0000

    Adding language information into accessibility statment controller for cookies banner

--- src/controllers/accessibilityStatementController.ts
+++ src/controllers/accessibilityStatementController.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import * as config from "../config";
+import { ACCESSIBILITY_STATEMENT, BASE_URL } from "../types/pageURL";
+import { getLocaleInfo, getLocalesService, selectLang } from "../utils/localise";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-    res.render(config.ACCESSIBILITY_STATEMENT);
+    const lang = selectLang(req.query.lang);
+    const locales = getLocalesService();
+    res.render(config.ACCESSIBILITY_STATEMENT, {
+        ...getLocaleInfo(locales, lang),
+        currentUrl: BASE_URL + ACCESSIBILITY_STATEMENT
+    });
 };
