591725bbf346c20dc52ff396a8af338edab87545
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-04-03 13:40:14 +0100

    Updating the routing on email address screen

--- src/controllers/features/update-acsp/whatIsYourEmailAddressController.ts
+++ src/controllers/features/update-acsp/whatIsYourEmailAddressController.ts
@@ -1,12 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import * as config from "../../../config";
-import { BASE_URL, UPDATE_WHAT_IS_YOUR_EMAIL, UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_YOUR_ANSWERS } from "../../../types/pageURL";
+import { UPDATE_WHAT_IS_YOUR_EMAIL, UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_YOUR_ANSWERS, UPDATE_CHECK_YOUR_UPDATES } from "../../../types/pageURL";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { validationResult } from "express-validator";
 import { Session } from "@companieshouse/node-session-handler";
 import { ACSP_DETAILS, ACSP_DETAILS_UPDATED } from "../../../common/__utils/constants";
 import { formatValidationError, getPageProperties } from "../../../validation/validation";
-import { saveDataInSession } from "../../../common/__utils/sessionHelper";
 import { AcspFullProfile } from "private-api-sdk-node/dist/services/acsp-profile/types";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
@@ -16,8 +15,6 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
         const session: Session = req.session as any as Session;
         const acspUpdatedFullProfile: AcspFullProfile = session.getExtraData(ACSP_DETAILS_UPDATED)!;
         const acspFullProfile: AcspFullProfile = session.getExtraData(ACSP_DETAILS)!;
-        const previousPage = addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang);
-        const currentUrl = BASE_URL + UPDATE_WHAT_IS_YOUR_EMAIL;
 
         let payload = {};
         if (acspUpdatedFullProfile.email === acspFullProfile.email) {
@@ -30,10 +27,11 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
                 whatIsYourEmailInput: acspUpdatedFullProfile.email
             };
         }
+
         res.render(config.UPDATE_WHAT_IS_YOUR_EMAIL, {
             ...getLocaleInfo(locales, lang),
-            previousPage,
-            currentUrl,
+            previousPage: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang),
+            currentUrl: UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_WHAT_IS_YOUR_EMAIL,
             correspondenceEmail: acspFullProfile.email,
             payload
         });
@@ -46,27 +44,28 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const lang = selectLang(req.query.lang);
         const locales = getLocalesService();
-        const errorList = validationResult(req);
         const session: Session = req.session as any as Session;
-        const previousPage = addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang);
         const acspUpdatedFullProfile: AcspFullProfile = session.getExtraData(ACSP_DETAILS_UPDATED)!;
         const acspFullProfile: AcspFullProfile = session.getExtraData(ACSP_DETAILS)!;
+        const errorList = validationResult(req);
         if (!errorList.isEmpty()) {
             const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
             res.status(400).render(config.UPDATE_WHAT_IS_YOUR_EMAIL, {
-                previousPage,
-                correspondenceEmail: acspFullProfile.email,
-                payload: req.body,
+                ...pageProperties,
                 ...getLocaleInfo(locales, lang),
+                previousPage: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang),
                 currentUrl: UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_WHAT_IS_YOUR_EMAIL,
-                ...pageProperties
+                correspondenceEmail: acspFullProfile.email,
+                payload: req.body
             });
         } else {
             if (req.body.whatIsYourEmailRadio === "A Different Email") {
                 acspUpdatedFullProfile.email = req.body.whatIsYourEmailInput;
-                saveDataInSession(req, ACSP_DETAILS_UPDATED, acspUpdatedFullProfile);
+                session.setExtraData(ACSP_DETAILS_UPDATED, acspUpdatedFullProfile);
+                res.redirect(addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_CHECK_YOUR_UPDATES, lang));
+            } else {
+                res.redirect(addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang));
             }
-            res.redirect(previousPage);
         }
     } catch (err) {
         next(err);
