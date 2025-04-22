b3fea6c68314dd5f1c4734407a62d09b3206d8d3
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-04-22 16:31:13 +0100

    Removing code smells and increasing test coverage

--- src/validation/whatIsYourName.ts
+++ src/validation/whatIsYourName.ts
@@ -9,12 +9,12 @@ const spaceFormat: RegExp = /\s+/g;
 export const nameValidator = [
     body("first-name").trim().notEmpty().withMessage("enterFirstName").bail().matches(nameFormat).withMessage("invalidFirstNameFormat").bail().isLength({ max: 50 })
         .withMessage("invalidFirstNameLength").bail().custom((value, { req }) => {
-            const session = req.session as any as Session;
+            const session: Session = req.session as Session;
             const acspDetails: AcspFullProfile | undefined = session.getExtraData(ACSP_DETAILS);
             if (acspDetails) {
-                const originalFirstName = normaliseName(acspDetails.soleTraderDetails?.forename);
-                const originalMiddleName = normaliseName(acspDetails.soleTraderDetails?.otherForenames);
-                const originalSurname = normaliseName(acspDetails.soleTraderDetails?.surname);
+                const originalFirstName = normaliseName(acspDetails.soleTraderDetails!.forename);
+                const originalMiddleName = normaliseName(acspDetails.soleTraderDetails!.otherForenames);
+                const originalSurname = normaliseName(acspDetails.soleTraderDetails!.surname);
 
                 if (originalFirstName === normaliseName(value) && originalMiddleName === normaliseName(req.body["middle-names"]) && originalSurname === normaliseName(req.body["last-name"])) {
                     throw new Error("noChangeFirstname");
@@ -24,12 +24,12 @@ export const nameValidator = [
         }),
     body("middle-names").trim().matches(nameFormat).withMessage("invalidMiddleNameFormat").bail().isLength({ max: 50 }).withMessage("invalidMiddleNameLength").bail()
         .custom((value, { req }) => {
-            const session = req.session as any as Session;
+            const session: Session = req.session as Session;
             const acspDetails: AcspFullProfile | undefined = session.getExtraData(ACSP_DETAILS);
             if (acspDetails) {
-                const originalFirstName = normaliseName(acspDetails.soleTraderDetails?.forename);
-                const originalMiddleName = normaliseName(acspDetails.soleTraderDetails?.otherForenames);
-                const originalSurname = normaliseName(acspDetails.soleTraderDetails?.surname);
+                const originalFirstName = normaliseName(acspDetails.soleTraderDetails!.forename);
+                const originalMiddleName = normaliseName(acspDetails.soleTraderDetails!.otherForenames);
+                const originalSurname = normaliseName(acspDetails.soleTraderDetails!.surname);
 
                 if (originalFirstName === normaliseName(req.body["first-name"]) && originalMiddleName === normaliseName(value) && originalSurname === normaliseName(req.body["last-name"])) {
                     throw new Error("noChangeMiddleNames");
@@ -39,12 +39,12 @@ export const nameValidator = [
         }),
     body("last-name").trim().notEmpty().withMessage("enterLastName").bail().matches(nameFormat).withMessage("invalidLastNameFormat").bail().isLength({ max: 160 })
         .withMessage("invalidLastNameLength").bail().custom((value, { req }) => {
-            const session = req.session as any as Session;
+            const session: Session = req.session as Session;
             const acspDetails: AcspFullProfile | undefined = session.getExtraData(ACSP_DETAILS);
             if (acspDetails) {
-                const originalFirstName = normaliseName(acspDetails.soleTraderDetails?.forename);
-                const originalMiddleName = normaliseName(acspDetails.soleTraderDetails?.otherForenames);
-                const originalSurname = normaliseName(acspDetails.soleTraderDetails?.surname);
+                const originalFirstName = normaliseName(acspDetails.soleTraderDetails!.forename);
+                const originalMiddleName = normaliseName(acspDetails.soleTraderDetails!.otherForenames);
+                const originalSurname = normaliseName(acspDetails.soleTraderDetails!.surname);
 
                 if (originalFirstName === normaliseName(req.body["first-name"]) && originalMiddleName === normaliseName(req.body["middle-names"]) && originalSurname === normaliseName(value)) {
                     throw new Error("noChangeLastName");
@@ -54,6 +54,9 @@ export const nameValidator = [
         })
 ];
 
-const normaliseName = (name: string | undefined): string | undefined => {
-    return name?.trim().toLowerCase().replace(spaceFormat, " ");
+const normaliseName = (name: string | undefined): string => {
+    if (!name) {
+        return "";
+    }
+    return name.trim().toLowerCase().replace(spaceFormat, " ");
 };
