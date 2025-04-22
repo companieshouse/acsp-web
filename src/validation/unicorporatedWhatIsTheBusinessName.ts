0bdddba7527a72e5686fa0715a9440929995ba44
cbroadley-ch <cbroadley@companieshouse.gov.uk>
2025-04-22 10:15:03 +0100

    adding validation for business name if no changes made

--- src/validation/unicorporatedWhatIsTheBusinessName.ts
+++ src/validation/unicorporatedWhatIsTheBusinessName.ts
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import { ACSP_DETAILS } from "../common/__utils/constants";
 
 const businessNameFormat:RegExp = /^[A-Za-z0-9\-&'.\s]*$/;
 
@@ -6,5 +7,22 @@ export const unicorporatedWhatIsTheBusinessNameValidator = [
 
     body("whatIsTheBusinessName").trim().notEmpty().withMessage("whatIsTheBusinessNameNoName").bail()
         .matches(businessNameFormat).withMessage("whatIsTheBusinessNameInvalidCharacters").bail()
-        .isLength({ max: 155 }).withMessage("whatIsTheBusinessNameCharactersLimit")
+        .isLength({ max: 155 }).withMessage("whatIsTheBusinessNameCharactersLimit").bail()
+        .custom((inputBusinessName, { req }) => {
+            // Custom validation used for Update ACSP Details service
+            // Check if the business name has changed through comparing the inputted business against the existing business name
+            const session = req.session as any;
+            const acspDetails = session.getExtraData(ACSP_DETAILS);
+
+            if (acspDetails) {
+                // Trim leading and trailing spaces, convert business name to lowercase, and replace multiple spaces with a single space
+                const normalisedBusinessName = inputBusinessName.trim().toLowerCase().replace(/\s+/g, " ");
+                const existingBusinessName = acspDetails.name.trim().toLowerCase().replace(/\s+/g, " ");
+
+                if (normalisedBusinessName === existingBusinessName) {
+                    throw new Error("whatIsTheBusinessNameNoChangeUpdateAcsp");
+                }
+            }
+            return true;
+        })
 ];
