11cd76a708191aad37d97fe91be28bbd866880a2
cbroadley-ch <cbroadley@companieshouse.gov.uk>
2025-04-24 17:13:12 +0100

    Adding ch register link as a constant

--- src/controllers/features/update-acsp/whatIsTheBusinessNameController.ts
+++ src/controllers/features/update-acsp/whatIsTheBusinessNameController.ts
@@ -5,7 +5,7 @@ import { formatValidationError, getPageProperties } from "../../../validation/va
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { Session } from "@companieshouse/node-session-handler";
 import { AcspFullProfile } from "private-api-sdk-node/dist/services/acsp-profile/types";
-import { ACSP_DETAILS_UPDATE_ELEMENT, ACSP_DETAILS_UPDATE_IN_PROGRESS, ACSP_DETAILS_UPDATED } from "../../../common/__utils/constants";
+import { ACSP_DETAILS_UPDATE_ELEMENT, ACSP_DETAILS_UPDATE_IN_PROGRESS, ACSP_DETAILS_UPDATED, CH_REGISTER_URL } from "../../../common/__utils/constants";
 import { UPDATE_WHAT_IS_THE_BUSINESS_NAME, UPDATE_YOUR_ANSWERS, UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_DATE_OF_THE_CHANGE } from "../../../types/pageURL";
 import { getBusinessName } from "../../../services/common";
 
@@ -20,7 +20,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
         };
         res.render(config.WHAT_IS_THE_BUSINESS_NAME, {
             typeOfBusiness: acspUpdatedFullProfile.type,
-            companiesHouseRegisterLink: "https://find-and-update.company-information.service.gov.uk/",
+            companiesHouseRegisterLink: CH_REGISTER_URL,
             previousPage: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang),
             ...getLocaleInfo(locales, lang),
             payload,
