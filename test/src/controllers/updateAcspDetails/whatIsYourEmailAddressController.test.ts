af55f905c2f2ff4c5e768783d5883adb22588fb6
maliv1-ch <mali@companieshouse.gov.uk>
2025-04-29 14:40:00 +0100

    IDVA5-2053 - Code coverage

--- test/src/controllers/updateAcspDetails/whatIsYourEmailAddressController.test.ts
+++ test/src/controllers/updateAcspDetails/whatIsYourEmailAddressController.test.ts
@@ -2,7 +2,8 @@ import mocks from "../../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../../src/app";
 import * as localise from "../../../../src/utils/localise";
-import { UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_YOUR_ANSWERS, UPDATE_WHAT_IS_YOUR_EMAIL, UPDATE_CHECK_YOUR_UPDATES } from "../../../../src/types/pageURL";
+import { UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_WHAT_IS_YOUR_EMAIL, UPDATE_CHECK_YOUR_UPDATES } from "../../../../src/types/pageURL";
+import { mockLimitedAcspFullProfile } from "../../../mocks/update_your_details.mock";
 
 jest.mock("@companieshouse/api-sdk-node");
 const router = supertest(app);
@@ -79,5 +80,37 @@ describe("POST " + UPDATE_WHAT_IS_YOUR_EMAIL, () => {
         const res = await router.post(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_WHAT_IS_YOUR_EMAIL);
         expect(res.status).toBe(500);
         expect(res.text).toContain("Sorry we are experiencing technical difficulties");
+    });
+
+    it("should return status 400 and display error message when no change has been made to ltd company name", async () => {
+        mocks.mockSessionMiddleware.mockImplementation((req, res, next) => {
+            req.session = {
+                getExtraData: jest.fn().mockReturnValue(mockLimitedAcspFullProfile)
+            };
+            next();
+        });
+        const res = await router.post(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_WHAT_IS_YOUR_EMAIL)
+            .send({
+                whatIsYourEmailRadio: "A Different Email",
+                whatIsYourEmailInput: "john.doe@example.com"
+            });
+        expect(res.status).toBe(400);
+        expect(res.text).toContain("You are already using that email address. Enter a different email address if it’s changed or cancel the update");
+    });
+
+    it("should return status 400 and display error message when no change has been made to ltd company name", async () => {
+        mocks.mockSessionMiddleware.mockImplementation((req, res, next) => {
+            req.session = {
+                getExtraData: jest.fn().mockReturnValue(mockLimitedAcspFullProfile)
+            };
+            next();
+        });
+        const res = await router.post(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_WHAT_IS_YOUR_EMAIL)
+            .send({
+                whatIsYourEmailRadio: "john.doe@example.com",
+                whatIsYourEmailInput: "john.doe@example.com"
+            });
+        expect(res.status).toBe(400);
+        expect(res.text).toContain("Select to enter a different email address if it’s changed or cancel the update");
     });
 });
