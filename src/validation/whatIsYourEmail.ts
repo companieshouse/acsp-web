9436b1da3895f3c47da4b5c86ca7d32062873c3f
maliv1-ch <mali@companieshouse.gov.uk>
2025-04-29 15:37:16 +0100

    IDVA5-2053 - Code review changes

--- src/validation/whatIsYourEmail.ts
+++ src/validation/whatIsYourEmail.ts
@@ -3,48 +3,37 @@ import { ACSP_DETAILS } from "../common/__utils/constants";
 import { body, ValidationChain } from "express-validator";
 import { Session } from "@companieshouse/node-session-handler";
 import { trimAndLowercaseString } from "../services/common";
+import { Request } from "express-validator/src/base";
 
-export type ProcessType = "registration" | "update";
+type EmailValidationType = "registration" | "update";
 
-export const whatIsYourEmailValidator = (type: ProcessType): ValidationChain[] => [
+export const whatIsYourEmailValidator = (type: EmailValidationType): ValidationChain[] => [
 
     body("whatIsYourEmailInput").trim().if(body("whatIsYourEmailRadio").equals("A Different Email"))
         .notEmpty().withMessage(type === "registration" ? "noEmail" : "noCorrespondenceEmail").bail()
-        .isEmail().withMessage("emailFormatIncorrect").bail().custom((input, { req }) => {
-            // Custom validation used for Update ACSP Details service,
-            // Check if the email has changed through comparing the inputted email against the existing email
-            const session : Session = req.session as Session;
-            const acspDetails: AcspFullProfile | undefined = session.getExtraData(ACSP_DETAILS);
+        .isEmail().withMessage("emailFormatIncorrect").bail()
+        .custom((input, { req }) => compareNewOldEmailChecker(req, input)).withMessage("emailNoChangeUpdateAcsp"),
 
-            if (acspDetails) {
-                // Trim leading and trailing spaces, convert email to lowercase
-                // and replace multiple spaces with a single space
-                const normalisedInput = trimAndLowercaseString(input);
-                const existingEmail = trimAndLowercaseString(acspDetails.email);
-                if (normalisedInput === existingEmail) {
-                    throw new Error("emailNoChangeUpdateAcsp");
-                }
-            } return true;
-        }),
-
-    body("whatIsYourEmailRadio", type === "registration" ? "noEmail" : "emailNoChange").notEmpty().bail().custom((value, { req }) => {
-
-        // Custom validation used for Update ACSP Details service,
-        // Check if the email has changed through comparing the inputted email against the existing email
-        const session : Session = req.session as Session;
-        const acspDetails: AcspFullProfile | undefined = session.getExtraData(ACSP_DETAILS);
-
-        if (acspDetails) {
-            // Trim leading and trailing spaces, convert email to lowercase
-            // and replace multiple spaces with a single space
-            const normalisedInput = trimAndLowercaseString(value);
-            const existingEmail = acspDetails.email;
-            // Check if the email has changed
-            // If the email is the same as the existing email, throw an error
-            if (normalisedInput === existingEmail) {
-                throw new Error("emailNoChange");
-            }
-        }
-        return true;
-    })
+    body("whatIsYourEmailRadio", type === "registration" ? "noEmail" : "emailNoChange").notEmpty().bail()
+        .custom((value, { req }) => compareNewOldEmailChecker(req, value)).withMessage("emailNoChange")
 ];
+
+const compareNewOldEmailChecker = (req: Request, value: string) => {
+    // Custom validation used for Update ACSP Details service,
+    // Check if the email has changed through comparing the inputted email against the existing email
+    const session : Session = req.session as Session;
+    const acspDetails: AcspFullProfile | undefined = session.getExtraData(ACSP_DETAILS);
+
+    if (acspDetails) {
+        // Trim leading and trailing spaces, convert email to lowercase
+        // and replace multiple spaces with a single space
+        const normalisedInput = trimAndLowercaseString(value);
+        const existingEmail = trimAndLowercaseString(acspDetails.email);
+        // Check if the email has changed
+        // If the email is the same as the existing email, throw an error
+        if (normalisedInput === existingEmail) {
+            throw new Error();
+        }
+    }
+    return true;
+};
