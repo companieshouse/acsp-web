c761e05c13bdd87950bc5a394cc47ab9a56d257c
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-05-09 13:38:33 +0100

    Updating confirm address screen to show the updated address on back button

--- src/controllers/features/update-acsp/correspondenceAddressConfirmController.ts
+++ src/controllers/features/update-acsp/correspondenceAddressConfirmController.ts
@@ -3,7 +3,8 @@ import * as config from "../../../config";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 import { UPDATE_CORRESPONDENCE_ADDRESS_CONFIRM, UPDATE_CORRESPONDENCE_ADDRESS_MANUAL, UPDATE_CORRESPONDENCE_ADDRESS_LOOKUP, UPDATE_ACSP_DETAILS_BASE_URL, UPDATE_YOUR_ANSWERS, UPDATE_DATE_OF_THE_CHANGE } from "../../../types/pageURL";
 import { Session } from "@companieshouse/node-session-handler";
-import { ACSP_UPDATE_PREVIOUS_PAGE_URL, ACSP_DETAILS_UPDATE_IN_PROGRESS } from "../../../common/__utils/constants";
+import { ACSP_UPDATE_PREVIOUS_PAGE_URL, ACSP_DETAILS_UPDATE_IN_PROGRESS, ACSP_DETAILS_UPDATED } from "../../../common/__utils/constants";
+import { AcspFullProfile } from "private-api-sdk-node/dist/services/acsp-profile/types";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -11,13 +12,23 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
         const locales = getLocalesService();
         const session: Session = req.session as any as Session;
 
+        let correspondenceAddress;
+        if (session.getExtraData(ACSP_DETAILS_UPDATE_IN_PROGRESS)) {
+            correspondenceAddress = session.getExtraData(ACSP_DETAILS_UPDATE_IN_PROGRESS);
+        } else {
+            const acspUpdatedFullProfile: AcspFullProfile = session.getExtraData(ACSP_DETAILS_UPDATED)!;
+            correspondenceAddress = acspUpdatedFullProfile.type === "sole-trader"
+                ? acspUpdatedFullProfile.registeredOfficeAddress
+                : acspUpdatedFullProfile.serviceAddress;
+        }
+
         res.render(config.CORRESPONDENCE_ADDRESS_CONFIRM, {
             previousPage: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_CORRESPONDENCE_ADDRESS_LOOKUP, lang),
             editPage: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_CORRESPONDENCE_ADDRESS_MANUAL, lang),
             cancelUpdateLink: addLangToUrl(UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_YOUR_ANSWERS, lang),
             ...getLocaleInfo(locales, lang),
             currentUrl: UPDATE_ACSP_DETAILS_BASE_URL + UPDATE_CORRESPONDENCE_ADDRESS_CONFIRM,
-            correspondenceAddress: session.getExtraData(ACSP_DETAILS_UPDATE_IN_PROGRESS)
+            correspondenceAddress
         });
     } catch (err) {
         next(err);
