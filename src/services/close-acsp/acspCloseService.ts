0b8e1f788e188e531f6e738a54aae107db5919a3
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-05-29 12:19:42 +0100

    Saving data to mongo for close acsp

--- src/services/close-acsp/acspCloseService.ts
+++ src/services/close-acsp/acspCloseService.ts
@@ -2,6 +2,9 @@ import { Session } from "@companieshouse/node-session-handler";
 import { CLOSE_DESCRIPTION, CLOSE_REFERENCE, CLOSE_SUBMISSION_ID } from "../../common/__utils/constants";
 import { postTransaction } from "../transactions/transaction_service";
 import logger from "../../utils/logger";
+import { postAcspRegistration } from "../../services/acspRegistrationService";
+import { AcspData } from "@companieshouse/api-sdk-node/dist/services/acsp";
+import { AcspFullProfile } from "private-api-sdk-node/dist/services/acsp-profile/types";
 
 export class AcspCloseService {
     async createTransaction (session: Session): Promise<void> {
@@ -15,6 +18,22 @@ export class AcspCloseService {
                 logger.error(`Error while creating transaction for close ACSP`);
                 return Promise.reject(error);
             }
+        }
+    }
+
+    async saveCloseDetails (session: Session, acspDetails: AcspFullProfile): Promise<void> {
+        try {
+            const transactionId: string = session.getExtraData(CLOSE_SUBMISSION_ID)!;
+
+            const acspData: AcspData = {
+                acspType: "CLOSE_ACSP",
+                acspId: acspDetails.number
+            };
+
+            await postAcspRegistration(session, transactionId, acspData);
+        } catch (error) {
+            logger.error("Error while saving close details for ACSP");
+            return Promise.reject(error);
         }
     }
 }
