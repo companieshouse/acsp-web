4896211c7257f33aad91c4c18625db420e251dce
hemerem-ch <hemerem@companieshouse.gov.uk>
2025-06-09 10:31:30 +0100

    Added test coverage for following changes "Set APP_NAME to acsp-web"

--- test/src/utils/logger.test.ts
+++ test/src/utils/logger.test.ts
@@ -6,9 +6,15 @@ const ERROR_MESSAGE = "Error: Something went wrong";
 logger.error = jest.fn();
 
 describe("logger tests", () => {
+    const ORIGINAL_ENV = process.env;
 
     beforeEach(() => {
         jest.clearAllMocks();
+        process.env = { ...ORIGINAL_ENV };
+    });
+
+    afterAll(() => {
+        process.env = ORIGINAL_ENV;
     });
 
     test("Should test the logger object is defined of type ApplicationLogger", () => {
@@ -20,5 +26,17 @@ describe("logger tests", () => {
         const err: Error = createAndLogError(ERROR_MESSAGE);
         expect(err.message).toEqual(ERROR_MESSAGE);
         expect(logger.error).toHaveBeenCalledTimes(1);
+    });
+
+    test("Should create logger using process.env.APP_NAME", () => {
+        process.env.APP_NAME = "test-app";
+
+        jest.resetModules();
+
+        const loggerModule = require("../../../src/utils/logger");
+        const reloadedLogger = loggerModule.default;
+
+        expect(reloadedLogger).toBeDefined();
+        expect(typeof reloadedLogger.error).toBe("function");
     });
 });
