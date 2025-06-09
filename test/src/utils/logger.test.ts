12062f932b427793b9e0fcd7d11a2d7b2066b7b1
hemerem-ch <hemerem@companieshouse.gov.uk>
2025-06-09 11:51:50 +0100

    Added test coverage for following changes "Set APP_NAME to acsp-web"

--- test/src/utils/logger.test.ts
+++ test/src/utils/logger.test.ts
@@ -1,9 +1,18 @@
 import ApplicationLogger from "@companieshouse/structured-logging-node/lib/ApplicationLogger";
-import logger, { createAndLogError } from "../../../src/utils/logger";
+import { createAndLogError } from "../../../src/utils/logger";
+import { warn } from "console";
 
 const ERROR_MESSAGE = "Error: Something went wrong";
 
-logger.error = jest.fn();
+jest.mock("@companieshouse/structured-logging-node", () => {
+    return {
+        createLogger: jest.fn(() => ({
+            error: jest.fn(),
+            info: jest.fn(),
+            warn: jest.fn()
+        }))
+    };
+});
 
 describe("logger tests", () => {
     const ORIGINAL_ENV = process.env;
@@ -17,15 +26,22 @@ describe("logger tests", () => {
         process.env = ORIGINAL_ENV;
     });
 
-    test("Should test the logger object is defined of type ApplicationLogger", () => {
-        expect(logger).toBeDefined();
-        expect(logger).toBeInstanceOf(ApplicationLogger);
-    });
-
     test("Should log and return an error", () => {
+        const mockLogger = require("../../../src/utils/logger").default;
         const err: Error = createAndLogError(ERROR_MESSAGE);
         expect(err.message).toEqual(ERROR_MESSAGE);
-        expect(logger.error).toHaveBeenCalledTimes(1);
+        expect(mockLogger.error).toHaveBeenCalledTimes(1);
+    });
+
+    test("Should test the logger object is defined of type ApplicationLogger", () => {
+        process.env.APP_NAME = "test-app";
+
+        jest.resetModules();
+        const loggerModule = require("../../../src/utils/logger");
+        const reloadedLogger = loggerModule.default;
+
+        expect(reloadedLogger).toBeDefined();
+        expect(typeof reloadedLogger.error).toBe("function");
     });
 
     test("Should create logger using process.env.APP_NAME", () => {
