ade048bd2f0e526d1464b2c7944b2248ecc2f0cf
rdas <rdas@companieshouse.gov.uk>
2025-06-11 17:20:10 +0100

    IDVA5-2197 Register an ACSP - Allow Previously Closed/ Ceased ACSPs to Access Registration Service --pusing again to run the sonar

--- src/services/checkSavedApplicationService.ts
+++ src/services/checkSavedApplicationService.ts
@@ -14,7 +14,7 @@ import logger from "../utils/logger";
 
 export const getRedirectionUrl = async (savedApplications: Resource<TransactionList>, session: Session): Promise<string> => {
     const transactions = filterRejectedApplications(savedApplications.resource!.items);
-    console.log("transactions", JSON.stringify(transactions));
+
     let url = "";
     if (!transactions.length) {
         logger.debug("application is rejected");
