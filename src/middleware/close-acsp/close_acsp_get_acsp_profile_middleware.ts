ed02e0cc8bd3455dc39c0068b77fcefdb94809f9
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-06-16 16:47:28 +0100

    Adding status check when user initally enters the service and gets the acsp profile

--- src/middleware/close-acsp/close_acsp_get_acsp_profile_middleware.ts
+++ src/middleware/close-acsp/close_acsp_get_acsp_profile_middleware.ts
@@ -2,8 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import { AcspFullProfile } from "private-api-sdk-node/dist/services/acsp-profile/types";
 import { getLoggedInAcspNumber } from "../../common/__utils/session";
 import { getAcspFullProfile } from "../../services/acspProfileService";
-import { ACSP_DETAILS } from "../../common/__utils/constants";
+import { ACSP_DETAILS, CEASED } from "../../common/__utils/constants";
 import { Session } from "@companieshouse/node-session-handler";
+import { AcspCeasedError } from "../../errors/acspCeasedError";
 
 export const getAcspProfileMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -14,6 +15,9 @@ export const getAcspProfileMiddleware = async (req: Request, res: Response, next
         if (!acspDetails) {
             acspDetails = await getAcspFullProfile(acspNumber);
             session.setExtraData(ACSP_DETAILS, acspDetails);
+            if (acspDetails.status === CEASED) {
+                throw new AcspCeasedError("ACSP is ceased. Cannot proceed with account closure.");
+            }
         }
         next();
     } catch (error) {
