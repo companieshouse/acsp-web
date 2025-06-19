84462087c91a29caa41336a907305d27e40f0876
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-06-19 10:54:37 +0100

    cannot-use-service-while-suspended files

---
+++ src/controllers/features/common/cannotUseServiceWhileSuspendedController.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { CANNOT_USE_SERVICE_WHILE_SUSPENDED } from "../../../types/pageURL";
+import { selectLang, getLocalesService, getLocaleInfo } from "../../../utils/localise";
+import * as config from "../../../config";
+
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+    const lang = selectLang(req.query.lang);
+    res.render(config.CANNOT_USE_SERVICE_WHILE_SUSPENDED, {
+        ...getLocaleInfo(getLocalesService(), lang),
+        currentUrl: CANNOT_USE_SERVICE_WHILE_SUSPENDED
+    });
+};
