a654fdfef4b9075821693cdd49e4d412c624a92c
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-08-14 14:38:44 +0100

    IDVA5-2428: Update special allowed chars for Business and Correspondence Address (#937)

--- src/validation/regexParts.ts
+++ src/validation/regexParts.ts
@@ -8,9 +8,9 @@ export const BUSINESS_NAME_EXCLUDED_CHARS = "\\^|~Ǻǻƒ";
 
 // Commmon allowed characters for name and business address/correspondence address
 export const ALLOWED_TEXT_CHARS_SUFFIX = "'\\s-";
-const ALLOWED_TEXT_CHARS_BASE = "A-ZŒÆæÀàÈèÌìÒòÙùẀẁỲỳÁáćǼǽÉéģÍíĹĺŃńÓóŔŕŚśÚúẂẃÝýŹźÂâĉÊêĜĝĤĥÎîĴĵÔôŜŝÛûŴŵŶŷÃãĨĩÑñÕõŨũÄäËëÏïÖöÜüẄẅŸÿÅåŮůa-zœçŊŋŞşŢţĢĶķĻļŅņŖŗĐĦħŦŧǾǿ";
+const ALLOWED_TEXT_CHARS_BASE = "A-ZŒÆæÀàÈèÌìÒòÙùẀẁỲỳÁáĆćǼǽÉéģÍíĹĺŃńÓóŔŕŚśÚúẂẃÝýŹźÂâĈĉÊêĜĝĤĥÎîĴĵÔôŜŝÛûŴŵŶŷÃãĨĩÑñÕõŨũÄäËëÏïÖöÜüẄẅŸÿÅåŮůa-zœçŊŋŞşŢţĢĶķĻļŅņŖŗĐĦħŦŧǾǿ";
 export const ALLOWED_TEXT_CHARS = `${ALLOWED_TEXT_CHARS_BASE}${ALLOWED_TEXT_CHARS_SUFFIX}`;
 
 // Allowed characters for ONLY business address/correspondence address
-const BUSINESS_ADDRESS_ALLOWED_CHARS_BASE = `${ALLOWED_TEXT_CHARS_BASE}0123456789Ǻǻƒ`;
+const BUSINESS_ADDRESS_ALLOWED_CHARS_BASE = `${ALLOWED_TEXT_CHARS_BASE}0123456789Ǻǻ`;
 export const BUSINESS_ADDRESS_ALLOWED_CHARS = `${BUSINESS_ADDRESS_ALLOWED_CHARS_BASE}${ALLOWED_TEXT_CHARS_SUFFIX}`;
