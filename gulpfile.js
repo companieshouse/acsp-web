e36362d6eadbd43ed3fb81783a7c59b561f58fe9
rdas <rdas@companieshouse.gov.uk>
2025-08-27 14:32:01 +0100

    IDVA5-2553 ACSP Web - Registration application navbar styling - added missed review comments

--- gulpfile.js
+++ gulpfile.js
@@ -47,13 +47,6 @@ gulp.task("js", () => {
         .pipe(gulp.dest(dstDirJs));
 });
 
-// Copy the fonts and images from the govuk-frontend package to the public directory
-gulp.task("govuk-assets", () => {
-    return gulp
-        .src(["./node_modules/govuk-frontend/dist/govuk/assets/**/*"])
-        .pipe(gulp.dest(dstDirAssets));
-});
-
 // Binding all tasks together...
 gulp.task("build", gulp.series(["clean", "sass", "js"]));
 
