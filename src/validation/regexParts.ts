2c08a04ed6774708f9d5771d9db41312ab38fe56
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-09-02 10:50:11 +0100

    Exclude Ǵ from business name

--- src/validation/regexParts.ts
+++ src/validation/regexParts.ts
@@ -4,7 +4,7 @@ export const NUMBERS = "\\p{N}"; // Any kind of digit
 export const PUNCTUATION = "\\p{P}";
 export const SYMBOLS = "\\p{S}";
 export const WHITESPACE = "\\s";
-export const BUSINESS_NAME_EXCLUDED_CHARS = "\\^|~Ǻǻƒ";
+export const BUSINESS_NAME_EXCLUDED_CHARS = "\\^|~ǺǻƒǴ";
 
 // Commmon allowed characters for name and business address/correspondence address
 export const ALLOWED_TEXT_CHARS_SUFFIX = "'\\s-";
